54b16f52759694aaa59ed1bc7927e3f7777c4080
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2021-05-12 17:16:59 -0400

    Heist splice for Pandoc (#4)
    * Prepare for pandocSplice implementation
    
    * Mostly working prototype
    
    * Finish impl
    
    * ...

--- src/Emabook/Template/Splices/Pandoc.hs
+++ src/Emabook/Template/Splices/Pandoc.hs
@@ -1,100 +1,140 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Emabook.Template.Splices.Pandoc (renderPandoc) where
+module Emabook.Template.Splices.Pandoc (pandocSplice) where
 
 import Control.Exception (throw)
 import qualified Data.Text as T
 import qualified Ema.Helper.Markdown as Markdown
+import qualified Heist as H
+import qualified Heist.Interpreted as HI
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Renderer.XmlHtml as RX
 import qualified Text.Pandoc.Builder as B
 import Text.Pandoc.Definition (Pandoc (..))
+import qualified Text.XmlHtml as XmlHtml
 
--- TODO: Use heist splice to override CSS classes
+-- | A splice to render a Pandoc AST allowing customization of the AST nodes in
+-- HTML.
+pandocSplice :: Monad n => Pandoc -> HI.Splice n
+pandocSplice doc = do
+  node <- H.getParamNode
+  pure $
+    RX.renderHtmlNodes $
+      renderPandocWith (blockLookupAttr node) (inlineLookupAttr node) doc
 
-renderPandoc :: Pandoc -> H.Html
-renderPandoc (Pandoc _meta blocks) =
-  mapM_ rpBlock blocks
+blockLookupAttr :: XmlHtml.Node -> B.Block -> B.Attr
+blockLookupAttr node = \case
+  B.Para {} -> childTagAttr node "Para"
+  B.BulletList {} -> childTagAttr node "BulletList"
+  B.OrderedList {} -> childTagAttr node "OrderedList"
+  B.CodeBlock {} -> childTagAttr node "CodeBlock"
+  B.Header level _ _ ->
+    fromMaybe B.nullAttr $ do
+      header <- XmlHtml.childElementTag "Header" node
+      pure $ childTagAttr header ("h" <> show level)
+  _ -> B.nullAttr
 
-rpBlock :: B.Block -> H.Html
-rpBlock = \case
+inlineLookupAttr :: XmlHtml.Node -> B.Inline -> B.Attr
+inlineLookupAttr node = \case
+  B.Link _ _ (url, _) ->
+    fromMaybe B.nullAttr $ do
+      link <- XmlHtml.childElementTag "PandocLink" node
+      let innerTag = if "://" `T.isInfixOf` url then "External" else "Internal"
+      pure $ attrFromNode link `addAttr` childTagAttr link innerTag
+  _ -> B.nullAttr
+
+childTagAttr :: XmlHtml.Node -> Text -> B.Attr
+childTagAttr x name =
+  maybe B.nullAttr attrFromNode $ XmlHtml.childElementTag name x
+
+attrFromNode :: XmlHtml.Node -> B.Attr
+attrFromNode node =
+  let mClass = maybe mempty T.words $ XmlHtml.getAttribute "class" node
+      id' = fromMaybe "" $ XmlHtml.getAttribute "id" node
+      attrs = filter ((/= "class") . fst) $ XmlHtml.elementAttrs node
+   in (id', mClass, attrs)
+
+renderPandocWith :: (B.Block -> B.Attr) -> (B.Inline -> B.Attr) -> Pandoc -> H.Html
+renderPandocWith bAttr iAttr (Pandoc _meta blocks) =
+  mapM_ (rpBlock bAttr iAttr) blocks
+
+rpBlock :: (B.Block -> B.Attr) -> (B.Inline -> B.Attr) -> B.Block -> H.Html
+rpBlock bAttr iAttr b = case b of
   B.Plain is ->
-    mapM_ rpInline is
+    mapM_ (rpInline iAttr) is
   B.Para is ->
-    H.p ! A.class_ "my-2" $ mapM_ rpInline is
+    H.p ! rpAttr (bAttr b) $ mapM_ (rpInline iAttr) is
   B.LineBlock iss ->
     forM_ iss $ \is ->
-      mapM_ rpInline is >> "\n"
+      mapM_ (rpInline iAttr) is >> "\n"
   B.CodeBlock (id', classes, attrs) s ->
-    -- Prism friendly classes
+    -- PrismJS friendly classes
     let classes' = flip concatMap classes $ \cls -> [cls, "language-" <> cls]
-     in H.div ! A.class_ "py-0.5 text-sm" $ H.pre ! rpAttr (id', classes', attrs) $ H.code ! rpAttr ("", classes', []) $ H.text s
+     in H.div ! rpAttr (bAttr b) $
+          H.pre ! rpAttr (id', classes', attrs) $
+            H.code ! rpAttr ("", classes', []) $ H.text s
   B.RawBlock (B.Format fmt) rawHtml ->
     if fmt == "html"
       then H.unsafeByteString $ encodeUtf8 rawHtml
       else throw Unsupported
   B.BlockQuote bs ->
-    H.blockquote $ mapM_ rpBlock bs
+    H.blockquote $ mapM_ (rpBlock bAttr iAttr) bs
   B.OrderedList _ bss ->
-    H.ol ! A.class_ (listStyle <> " list-decimal") $
+    H.ol ! rpAttr (bAttr b) $
       forM_ bss $ \bs ->
-        H.li ! A.class_ listItemStyle $ mapM_ rpBlock bs
+        H.li $ mapM_ (rpBlock bAttr iAttr) bs
   B.BulletList bss ->
-    H.ul ! A.class_ (listStyle <> " list-disc") $
+    H.ul ! rpAttr (bAttr b) $
       forM_ bss $ \bs ->
-        H.li ! A.class_ listItemStyle $ mapM_ rpBlock bs
+        H.li $ mapM_ (rpBlock bAttr iAttr) bs
   B.DefinitionList defs ->
     H.dl $
       forM_ defs $ \(term, descList) -> do
-        mapM_ rpInline term
+        mapM_ (rpInline iAttr) term
         forM_ descList $ \desc ->
-          H.dd $ mapM_ rpBlock desc
+          H.dd $ mapM_ (rpBlock bAttr iAttr) desc
   B.Header level attr is ->
-    headerElem level ! rpAttr attr $ mapM_ rpInline is
+    headerElem level ! rpAttr (addAttr attr $ bAttr b) $ mapM_ (rpInline iAttr) is
   B.HorizontalRule ->
     H.hr
   B.Table {} ->
     throw Unsupported
   B.Div attr bs ->
-    H.div ! rpAttr attr $ mapM_ rpBlock bs
+    H.div ! rpAttr attr $ mapM_ (rpBlock bAttr iAttr) bs
   B.Null ->
     pure ()
-  where
-    listStyle = "list-inside ml-4 space-y-1 "
-    listItemStyle = ""
 
 headerElem :: Int -> H.Html -> H.Html
 headerElem = \case
-  1 -> H.h1 ! A.class_ "text-6xl mt-2 mb-2 text-center pb-2"
-  2 -> H.h2 ! A.class_ ("text-5xl " <> my)
-  3 -> H.h3 ! A.class_ ("text-4xl " <> my)
-  4 -> H.h4 ! A.class_ ("text-3xl " <> my)
-  5 -> H.h5 ! A.class_ ("text-2xl " <> my)
-  6 -> H.h6 ! A.class_ ("text-xl " <> my)
+  1 -> H.h1
+  2 -> H.h2
+  3 -> H.h3
+  4 -> H.h4
+  5 -> H.h5
+  6 -> H.h6
   _ -> error "Invalid pandoc header level"
-  where
-    my = "mt-4 mb-2 text-gray-700"
 
-rpInline :: B.Inline -> H.Html
-rpInline = \case
+rpInline :: (B.Inline -> B.Attr) -> B.Inline -> H.Html
+rpInline iAttr i = case i of
   B.Str s -> H.toHtml s
   B.Emph is ->
-    H.em $ mapM_ rpInline is
+    H.em $ mapM_ (rpInline iAttr) is
   B.Strong is ->
-    H.strong $ mapM_ rpInline is
+    H.strong $ mapM_ (rpInline iAttr) is
   B.Underline is ->
-    H.u $ mapM_ rpInline is
+    H.u $ mapM_ (rpInline iAttr) is
   B.Strikeout is ->
     -- FIXME: Should use <s>, but blaze doesn't have it.
-    H.del $ mapM_ rpInline is
+    H.del $ mapM_ (rpInline iAttr) is
   B.Superscript is ->
-    H.sup $ mapM_ rpInline is
+    H.sup $ mapM_ (rpInline iAttr) is
   B.Subscript is ->
-    H.sub $ mapM_ rpInline is
+    H.sub $ mapM_ (rpInline iAttr) is
   B.Quoted qt is ->
-    flip inQuotes qt $ mapM_ rpInline is
+    flip inQuotes qt $ mapM_ (rpInline iAttr) is
   B.Code attr s ->
     H.code ! rpAttr attr $ H.toHtml s
   B.Space -> " "
@@ -105,23 +145,17 @@ rpInline = \case
   B.Math _ _ ->
     throw Unsupported
   B.Link attr is (url, tit) -> do
-    let (cls, target) =
-          if "://" `T.isInfixOf` url
-            then ("text-pink-600 hover:underline", targetBlank)
-            else ("text-pink-600 font-bold hover:bg-pink-50", mempty)
     H.a
-      ! A.class_ cls
       ! A.href (H.textValue url)
       ! A.title (H.textValue tit)
-      ! target
-      ! rpAttr attr
-      $ mapM_ rpInline is
+      ! rpAttr (addAttr attr $ iAttr i)
+      $ mapM_ (rpInline iAttr) is
   B.Image attr is (url, tit) ->
     H.img ! A.src (H.textValue url) ! A.title (H.textValue tit) ! A.alt (H.textValue $ Markdown.plainify is) ! rpAttr attr
   B.Note _ ->
     throw Unsupported
   B.Span attr is ->
-    H.span ! rpAttr attr $ mapM_ rpInline is
+    H.span ! rpAttr attr $ mapM_ (rpInline iAttr) is
   x ->
     H.pre $ H.toHtml $ show @Text x
   where
@@ -130,19 +164,23 @@ rpInline = \case
       B.SingleQuote -> "‘" >> w <* "’"
       B.DoubleQuote -> "“" >> w <* "”"
 
-targetBlank :: H.Attribute
-targetBlank =
-  A.target "_blank" <> A.rel "noopener"
-
 rpAttr :: B.Attr -> H.Attribute
 rpAttr (id', classes, attrs) =
   let cls = T.intercalate " " classes
    in unlessNull id' (A.id (fromString . toString $ id'))
         <> unlessNull cls (A.class_ (fromString . toString $ cls))
-        <> mconcat (fmap (\(k, v) -> H.dataAttribute (fromString . toString $ k) (fromString . toString $ v)) attrs)
+        <> mconcat (fmap (\(k, v) -> H.customAttribute (fromString . toString $ k) (fromString . toString $ v)) attrs)
   where
     unlessNull x f =
       if T.null x then mempty else f
+
+addAttr :: B.Attr -> B.Attr -> B.Attr
+addAttr (id1, cls1, attr1) (id2, cls2, attr2) =
+  (pickNonNull id1 id2, cls1 <> cls2, attr1 <> attr2)
+  where
+    pickNonNull x "" = x
+    pickNonNull "" x = x
+    pickNonNull _ _ = ""
 
 data Unsupported = Unsupported
   deriving (Show, Exception)
