7a9ec9a536f2aaa969b235cc76fcd5b09069dea8
Sridhar Ratnakumar <srid@srid.ca>
2021-05-15 23:17:08 -0400

    sidebar template: simpify tags

--- docs/.emabook/templates/components/sidebar-tree.tpl
+++ docs/.emabook/templates/components/sidebar-tree.tpl
@@ -1,11 +1,7 @@
 <!-- TODO: A way to disable collapsing; ideally on per node basis.-->
 <ema:route-tree>
-  <tree class="pl-2" />
-  <tree:inactive class="hidden" />
-  <tree:active class="bg-${theme}-100" />
+  <tree class="pl-2" inactive:class="hidden" active:class="bg-${theme}-100" />
   <!-- Make this an inner splice, so as to render folder icon-->
-  <item:parent class="my-2" />
-  <item:terminal class="my-2 text-gray-600" />
-  <link:active class="font-bold text-${theme}-600 hover:text-black" />
-  <link:inactive class="hover:text-black" />
+  <item parent:class="my-2" terminal:class="my-2 text-gray-600" />
+  <link active:class="font-bold text-${theme}-600 hover:text-black" inactive:class="hover:text-black" />
 </ema:route-tree>
--- src/Emabook/Template/Splices/Tree.hs
+++ src/Emabook/Template/Splices/Tree.hs
@@ -7,7 +7,6 @@
 module Emabook.Template.Splices.Tree (treeSplice, TreeLoc (..)) where
 
 import qualified Data.List.NonEmpty as NE
-import qualified Data.Map.Strict as Map
 import qualified Data.Text as T
 import Data.Tree (Tree (Node))
 import Ema (Ema (..))
@@ -20,17 +19,6 @@ import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Renderer.XmlHtml as RX
 import qualified Text.Show as Show
 import qualified Text.XmlHtml as XmlHtml
-
-data TreeLoc
-  = -- | Location is exactly pointing to current active route
-    TreeLoc_Current
-  | -- | Location is an ancestor of current active route
-    TreeLoc_Ancestor
-  | -- | Location is direct child of current active route
-    TreeLoc_Child
-  | -- | Elsewhere in tree.
-    TreeLoc_Elsewhere
-  deriving (Eq, Show, Ord)
 
 -- | Heist splice to render a `Data.Tree` whilst allowing customization of
 -- individual styling in templates.
@@ -49,32 +37,31 @@ treeSplice ::
   HI.Splice n
 treeSplice tree pathToRoute getTreeLoc itemRender = do
   node <- H.getParamNode
-  let getChildClass name =
-        XmlHtml.getAttribute "class" <=< XmlHtml.childElementTag name $ node
-      classes =
-        Map.mapMaybe id $
-          Map.fromList $
-            (id &&& getChildClass)
-              <$> mconcat
-                [ fmap show [minBound @TreeSub .. maxBound],
-                  fmap show [minBound @TreeItemState .. maxBound],
-                  fmap show [minBound @TreeLinkState .. maxBound]
-                ]
-  pure $ RX.renderHtmlNodes $ go getChildClass classes [] tree
+  let getChildTagClass tag extra = do
+        child <- XmlHtml.childElementTag tag node
+        nonEmpty $
+          catMaybes $
+            [ XmlHtml.getAttribute "class" child
+            ]
+              <> ( extra
+                     <&> \prefix ->
+                       XmlHtml.getAttribute (prefix <> ":class") child
+                 )
+  pure $ RX.renderHtmlNodes $ go getChildTagClass [] tree
   where
-    go getChildCls attrs parSlugs (xs :: [Tree a]) = do
-      let childClsAttr tags =
-            A.class_ $ H.toValue $ T.intercalate " " $ mapMaybe getChildCls tags
+    go getChildCls parSlugs (xs :: [Tree a]) = do
+      let childClsAttr tag states =
+            maybe mempty (A.class_ . H.toValue . T.intercalate " " . toList) $
+              getChildCls tag states
           subTreeCls =
-            childClsAttr $
-              [ show TreeSub,
-                show TreeSub <> ":level:" <> show (length parSlugs)
+            childClsAttr "tree" $
+              [ "level:" <> show (length parSlugs)
               ]
                 <> case getTreeLoc <$> nonEmpty (reverse parSlugs) of
-                  Just TreeLoc_Current -> ["tree:active", "tree:active:current"]
-                  Just TreeLoc_Ancestor -> ["tree:active", "tree:active:ancestor"]
-                  Just TreeLoc_Child -> ["tree:active", "tree:active:child"]
-                  Just TreeLoc_Elsewhere -> ["tree:inactive"]
+                  Just TreeLoc_Current -> ["active", "active:current"]
+                  Just TreeLoc_Ancestor -> ["active", "active:ancestor"]
+                  Just TreeLoc_Child -> ["active", "active:child"]
+                  Just TreeLoc_Elsewhere -> ["inactive"]
                   Nothing -> []
       H.div ! subTreeCls $ do
         forM_ xs $ \(Node slug children) -> do
@@ -83,29 +70,28 @@ treeSplice tree pathToRoute getTreeLoc itemRender = do
                 if null parSlugs || not (null children)
                   then TreeItem_RootOrParent
                   else TreeItem_Terminal
-              itemCls = classFrom attrs (one $ show itemState)
+              itemCls = childClsAttr "item" (one $ show itemState)
               itemRoute = pathToRoute $ NE.reverse $ slug :| parSlugs
               linkState =
                 case getTreeLoc itemPath of
                   TreeLoc_Current -> TreeLink_Active
                   _ -> TreeLink_Inactive
-              linkCls = classFrom attrs (one $ show linkState)
+              linkCls = childClsAttr "link" (one $ show linkState)
           H.div ! itemCls $
             H.a ! linkCls ! A.href (H.toValue $ Ema.routeUrl itemRoute) $
               itemRender itemRoute
-          go getChildCls attrs ([slug] <> parSlugs) children
-    classFrom attrs ks =
-      classFromElse attrs ks Nothing
-    classFromElse attrs ks (mv :: Maybe Text) =
-      let vs = mapMaybe (`Map.lookup` attrs) ks
-          mCls = maybe mv (Just . T.intercalate " " . toList) $ nonEmpty vs
-       in maybe mempty (A.class_ . H.toValue) mCls
+          go getChildCls ([slug] <> parSlugs) children
 
-data TreeSub = TreeSub
-  deriving (Eq, Enum, Bounded)
-
-instance Show TreeSub where
-  show TreeSub = "tree"
+data TreeLoc
+  = -- | Location is exactly pointing to current active route
+    TreeLoc_Current
+  | -- | Location is an ancestor of current active route
+    TreeLoc_Ancestor
+  | -- | Location is direct child of current active route
+    TreeLoc_Child
+  | -- | Elsewhere in tree.
+    TreeLoc_Elsewhere
+  deriving (Eq, Show, Ord)
 
 data TreeItemState
   = TreeItem_RootOrParent
@@ -114,8 +100,8 @@ data TreeItemState
 
 instance Show TreeItemState where
   show = \case
-    TreeItem_RootOrParent -> "item:parent"
-    TreeItem_Terminal -> "item:terminal"
+    TreeItem_RootOrParent -> "parent"
+    TreeItem_Terminal -> "terminal"
 
 data TreeLinkState
   = TreeLink_Active
@@ -124,5 +110,5 @@ data TreeLinkState
 
 instance Show TreeLinkState where
   show = \case
-    TreeLink_Active -> "link:active"
-    TreeLink_Inactive -> "link:inactive"
+    TreeLink_Active -> "active"
+    TreeLink_Inactive -> "inactive"
