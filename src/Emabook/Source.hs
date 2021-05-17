b5825e77d297ec2db423fdd50a940c9b161b1715
Sridhar Ratnakumar <srid@srid.ca>
2021-05-17 15:36:08 -0400

    Put Source in its own module

---
+++ src/Emabook/Source.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Emabook.Source where
+
+import Control.Exception (throw)
+import Control.Lens.Operators ((.~))
+import Control.Monad.Logger
+import qualified Data.Aeson as Aeson
+import qualified Data.Yaml as Yaml
+import qualified Ema.Helper.FileSystem as FileSystem
+import qualified Ema.Helper.Markdown as Markdown
+import Emabook.Model (Model)
+import qualified Emabook.Model as M
+import Emabook.Route (MarkdownRoute)
+import qualified Emabook.Route as R
+import qualified Emabook.Route.Ext as Ext
+import qualified Emabook.Template as T
+import System.FilePath ((</>))
+
+log :: MonadLogger m => Text -> m ()
+log = logInfoNS "emabook"
+
+logD :: MonadLogger m => Text -> m ()
+logD = logDebugNS "emabook"
+
+logE :: MonadLogger m => Text -> m ()
+logE = logErrorNS "emabook"
+
+-- | Represents the different kinds of file the application will handle.
+data Source
+  = -- | Markdown file
+    SourceMarkdown
+  | -- | YAML data file
+    SourceData
+  | -- | Heist template file
+    SourceTemplate FilePath
+  deriving (Eq, Show)
+
+sourcePattern :: Source -> FilePath
+sourcePattern = \case
+  SourceMarkdown -> "**/*.md"
+  SourceData -> "**/*.yaml"
+  SourceTemplate dir -> dir </> "**/*.tpl"
+
+filePatterns :: [(Source, FilePath)]
+filePatterns =
+  (id &&& sourcePattern)
+    <$> [ SourceMarkdown,
+          SourceData,
+          SourceTemplate ".emabook/templates"
+        ]
+
+-- | Transform a filesystem action (on a source) to model update
+transformAction :: (MonadIO m, MonadLogger m) => Source -> FilePath -> FileSystem.FileAction -> m (Model -> Model)
+transformAction src fp action =
+  case src of
+    SourceMarkdown -> case action of
+      FileSystem.Update ->
+        fmap (fromMaybe id) . runMaybeT $ do
+          r :: MarkdownRoute <- MaybeT $ pure $ R.mkRouteFromFilePath @Ext.Md fp
+          logD $ "Reading note " <> toText fp
+          !s <- readFileText fp
+          (mMeta, doc) <- either (throw . BadInput) pure $ parseMarkdown fp s
+          pure $ M.modelInsert r (fromMaybe Aeson.Null mMeta, doc)
+      FileSystem.Delete ->
+        pure $ maybe id M.modelDelete (R.mkRouteFromFilePath @Ext.Md fp)
+    SourceData -> case action of
+      FileSystem.Update -> do
+        fmap (fromMaybe id) . runMaybeT $ do
+          r :: R.Route Ext.Yaml <- MaybeT $ pure $ R.mkRouteFromFilePath @Ext.Yaml fp
+          logD $ "Reading data " <> toText fp
+          !s <- readFileBS fp
+          sdata <-
+            either (throw . BadInput . show) pure $
+              Yaml.decodeEither' s
+          pure $ M.modelInsertData r sdata
+      FileSystem.Delete ->
+        pure $ maybe id M.modelDeleteData (R.mkRouteFromFilePath @Ext.Yaml fp)
+    SourceTemplate dir ->
+      (M.modelHeistTemplate .~) <$> T.loadHeistTemplates dir
+  where
+    parseMarkdown =
+      Markdown.parseMarkdownWithFrontMatter @Aeson.Value $
+        Markdown.wikilinkSpec <> Markdown.fullMarkdownSpec
+
+newtype BadInput = BadInput Text
+  deriving (Show, Exception)
--- src/Main.hs
+++ src/Main.hs
@@ -8,20 +8,15 @@
 
 module Main where
 
-import Control.Exception (throw)
-import Control.Lens.Operators ((.~), (^.))
-import Control.Monad.Logger
-import qualified Data.Aeson as Aeson
+import Control.Lens.Operators ((^.))
 import qualified Data.List.NonEmpty as NE
 import Data.Map.Syntax ((##))
 import qualified Data.Map.Syntax as MapSyntax
 import qualified Data.Text as T
-import qualified Data.Yaml as Yaml
 import Ema (Ema)
 import qualified Ema
 import qualified Ema.CLI
 import qualified Ema.Helper.FileSystem as FileSystem
-import qualified Ema.Helper.Markdown as Markdown
 import qualified Ema.Helper.PathTree as PathTree
 import Emabook.Model (Model)
 import qualified Emabook.Model as M
@@ -33,6 +28,7 @@ import Emabook.Route (MarkdownRoute)
 import qualified Emabook.Route as R
 import qualified Emabook.Route.Ext as Ext
 import qualified Emabook.Route.WikiLinkTarget as WL
+import qualified Emabook.Source as Source
 import qualified Emabook.Template as T
 import qualified Emabook.Template.Splices.List as Splices
 import qualified Emabook.Template.Splices.Pandoc as Splices
@@ -42,34 +38,12 @@ import qualified Heist.Splices as Heist
 import qualified Heist.Splices.Apply as HA
 import qualified Heist.Splices.Bind as HB
 import qualified Heist.Splices.Json as HJ
-import System.FilePath ((</>))
 import qualified Text.Pandoc.Builder as B
 import Text.Pandoc.Definition (Pandoc (..))
 
 -- ------------------------
 -- Main entry point
 -- ------------------------
-
-log :: MonadLogger m => Text -> m ()
-log = logInfoNS "emabook"
-
-logD :: MonadLogger m => Text -> m ()
-logD = logDebugNS "emabook"
-
-logE :: MonadLogger m => Text -> m ()
-logE = logErrorNS "emabook"
-
-data Source
-  = SourceMarkdown
-  | SourceData
-  | SourceTemplate FilePath
-  deriving (Eq, Show)
-
-sourcePattern :: Source -> FilePath
-sourcePattern = \case
-  SourceMarkdown -> "**/*.md"
-  SourceData -> "**/*.yaml"
-  SourceTemplate dir -> dir </> "**/*.tpl"
 
 instance Ema Model MarkdownRoute where
   encodeRoute = R.encodeRoute
@@ -81,45 +55,8 @@ instance Ema Model MarkdownRoute where
 main :: IO ()
 main =
   Ema.runEma render $ \model -> do
-    let pats =
-          (id &&& sourcePattern)
-            <$> [ SourceMarkdown,
-                  SourceData,
-                  SourceTemplate ".emabook/templates"
-                ]
-    FileSystem.mountOnLVar "." pats model $ \(src, fp) action ->
-      case src of
-        SourceMarkdown -> case action of
-          FileSystem.Update ->
-            fmap (fromMaybe id) . runMaybeT $ do
-              r :: MarkdownRoute <- MaybeT $ pure $ R.mkRouteFromFilePath @Ext.Md fp
-              logD $ "Reading note " <> toText fp
-              !s <- readFileText fp
-              (mMeta, doc) <- either (throw . BadInput) pure $ parseMarkdown fp s
-              pure $ M.modelInsert r (fromMaybe Aeson.Null mMeta, doc)
-          FileSystem.Delete ->
-            pure $ maybe id M.modelDelete (R.mkRouteFromFilePath @Ext.Md fp)
-        SourceData -> case action of
-          FileSystem.Update -> do
-            fmap (fromMaybe id) . runMaybeT $ do
-              r :: R.Route Ext.Yaml <- MaybeT $ pure $ R.mkRouteFromFilePath @Ext.Yaml fp
-              logD $ "Reading data " <> toText fp
-              !s <- readFileBS fp
-              sdata <-
-                either (throw . BadInput . show) pure $
-                  Yaml.decodeEither' s
-              pure $ M.modelInsertData r sdata
-          FileSystem.Delete ->
-            pure $ maybe id M.modelDeleteData (R.mkRouteFromFilePath @Ext.Yaml fp)
-        SourceTemplate dir ->
-          (M.modelHeistTemplate .~) <$> T.loadHeistTemplates dir
-  where
-    parseMarkdown =
-      Markdown.parseMarkdownWithFrontMatter @Aeson.Value $
-        Markdown.wikilinkSpec <> Markdown.fullMarkdownSpec
-
-newtype BadInput = BadInput Text
-  deriving (Show, Exception)
+    FileSystem.mountOnLVar "." Source.filePatterns model $ \(src, fp) action ->
+      Source.transformAction src fp action
 
 render :: Ema.CLI.Action -> Model -> MarkdownRoute -> LByteString
 render _ model r = do
