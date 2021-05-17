53c5e7029034e1d981c1749efe25b1bf64cbd9c1
Sridhar Ratnakumar <srid@srid.ca>
2021-05-17 15:22:20 -0400

    Move Meta handling to its own module

--- src/Emabook/Model.hs
+++ src/Emabook/Model.hs
@@ -9,13 +9,10 @@ module Emabook.Model where
 
 import Control.Lens.Operators as Lens ((%~), (^.))
 import Control.Lens.TH (makeLenses)
-import Data.Aeson (FromJSON)
 import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Extra.Merge as AesonMerge
 import Data.Default (Default (..))
 import Data.IxSet.Typed ((@+), (@=))
 import qualified Data.IxSet.Typed as Ix
-import qualified Data.List.NonEmpty as NE
 import qualified Data.Set as Set
 import Data.Tree (Tree)
 import Ema (Slug)
@@ -24,19 +21,17 @@ import Emabook.Model.Note
   ( IxNote,
     Note (Note),
     SelfRef (SelfRef),
-    noteMeta,
     noteRoute,
     noteTitle,
   )
 import Emabook.Model.Rel (IxRel)
 import qualified Emabook.Model.Rel as Rel
-import Emabook.Model.SData (IxSData, SData (SData), sdataValue)
+import Emabook.Model.SData (IxSData, SData (SData))
 import Emabook.Route (MarkdownRoute)
 import qualified Emabook.Route as R
 import qualified Emabook.Route.Ext as Ext
 import qualified Emabook.Route.WikiLinkTarget as WL
 import qualified Emabook.Template as T
-import Relude.Extra.Map (StaticMap (lookup))
 import Text.Pandoc.Definition (Pandoc (..))
 import qualified Text.Pandoc.Definition as B
 
@@ -60,17 +55,6 @@ modelLookup :: MarkdownRoute -> Model -> Maybe Note
 modelLookup k =
   Ix.getOne . Ix.getEQ k . _modelNotes
 
-lookupNoteMeta :: (Default a, FromJSON a) => a -> Text -> MarkdownRoute -> Model -> a
-lookupNoteMeta x k r model =
-  fromMaybe x $ do
-    Aeson.Object obj <- pure $ modelComputeMeta r model
-    resultToMaybe . Aeson.fromJSON =<< lookup k obj
-  where
-    resultToMaybe :: Aeson.Result b -> Maybe b
-    resultToMaybe = \case
-      Aeson.Error _ -> Nothing
-      Aeson.Success b -> pure b
-
 modelLookupRouteByWikiLink :: WL.WikiLinkTarget -> Model -> [MarkdownRoute]
 modelLookupRouteByWikiLink wl model =
   -- TODO: Also lookup wiki links to *directories* without an associated zettel.
@@ -90,28 +74,6 @@ modelLookupBacklinks r model =
 modelLookupTitle :: MarkdownRoute -> Model -> Text
 modelLookupTitle r =
   maybe (R.routeFileBase r) noteTitle . modelLookup r
-
--- | Get the (final) metadata of a note, by merging it with the defaults
--- specified in parent routes all the way upto index.yaml.
-modelComputeMeta :: MarkdownRoute -> Model -> Aeson.Value
-modelComputeMeta mr model =
-  -- NOTE: This should never return Aeson.Null as long there is an index.yaml
-  -- TODO: Capture and warn of this invariant in user-friendly way.
-  fromMaybe Aeson.Null $ do
-    let defaultFiles = R.routeInits @Ext.Yaml (coerce mr)
-    defaults <- nonEmpty $
-      flip mapMaybe (toList defaultFiles) $ \r -> do
-        v <- fmap (^. sdataValue) . Ix.getOne . Ix.getEQ r $ model ^. modelData
-        guard $ v /= Aeson.Null
-        pure v
-    let finalDefault = NE.last $ NE.scanl1 mergeAeson defaults
-    pure $
-      fromMaybe finalDefault $ do
-        frontmatter <- (^. noteMeta) <$> modelLookup mr model
-        guard $ frontmatter /= Aeson.Null -- To not trip up AesonMerge
-        pure $ mergeAeson finalDefault frontmatter
-  where
-    mergeAeson = AesonMerge.lodashMerge
 
 modelInsertData :: R.Route Ext.Yaml -> Aeson.Value -> Model -> Model
 modelInsertData r v =
