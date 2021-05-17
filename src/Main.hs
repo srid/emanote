39d132541f3699170db17c322daf389f75d24632
Sridhar Ratnakumar <srid@srid.ca>
2021-05-17 15:03:47 -0400

    Move url resolution back to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -15,6 +15,7 @@ import qualified Data.Aeson as Aeson
 import qualified Data.List.NonEmpty as NE
 import Data.Map.Syntax ((##))
 import qualified Data.Map.Syntax as MapSyntax
+import qualified Data.Text as T
 import qualified Data.Yaml as Yaml
 import Ema (Ema)
 import qualified Ema
@@ -25,6 +26,8 @@ import qualified Ema.Helper.PathTree as PathTree
 import Emabook.Model (Model)
 import qualified Emabook.Model as M
 import qualified Emabook.Model.Note as MN
+import qualified Emabook.Model.Rel as Rel
+import qualified Emabook.PandocUtil as PandocUtil
 import Emabook.Route (MarkdownRoute)
 import qualified Emabook.Route as R
 import qualified Emabook.Template as T
@@ -170,5 +173,30 @@ render _ model r = do
           -- TODO: Display folder children if this is a folder note. It is hinted to in the sidebar too.
           Pandoc mempty $ one $ B.Plain $ one $ B.Str "No Markdown file exists for this route."
         Just note ->
-          -- TODO: Need to handle broken links somehow.
-          M.sanitizeMarkdown model r $ note ^. MN.noteDoc
+          note ^. MN.noteDoc
+            & ( PandocUtil.withoutH1 -- Because, handling note title separately
+                  >>> PandocUtil.rewriteLinks (resolveUrl model)
+              )
+
+-- | Convert .md or wiki links to their proper route url.
+--
+-- Requires resolution from the `model` state. Late resolution, in other words.
+resolveUrl :: Model -> Text -> Text
+resolveUrl model url =
+  fromMaybe url $ do
+    guard $ not $ isStaticAssetUrl url
+    Rel.parseUrl url >>= \case
+      Right r -> do
+        pure $ Ema.routeUrl r
+      Left wl ->
+        case nonEmpty (M.modelLookupRouteByWikiLink wl model) of
+          Nothing -> do
+            -- TODO: Set an attribute for broken links, so templates can style it accordingly
+            let fakeRouteUnder404 = R.Route @R.Md $ one "404" <> R.unWikiLinkText wl
+            pure $ Ema.routeUrl fakeRouteUnder404
+          Just targets ->
+            -- TODO: Deal with ambiguous targets here
+            pure $ Ema.routeUrl $ head targets
+  where
+    isStaticAssetUrl s =
+      any (\asset -> ("/" <> toText asset) `T.isPrefixOf` s) (Ema.staticAssets $ Proxy @MarkdownRoute)
