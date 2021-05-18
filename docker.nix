7d306ccfe6ebb8c6b45e502942e62a00de0ce0d6
Sridhar Ratnakumar <srid@srid.ca>
2021-05-18 19:08:06 -0400

    Build docker image in CI

---
+++ docker.nix
@@ -0,0 +1,41 @@
+# Builds a docker image containing the emanote executable
+#
+# Run as:
+#   docker load -i $(
+#     nix-build docker.nix \
+#       --argstr name <image-name> \
+#       --argstr tag <image-tag>
+#   )
+let
+  pkgs =
+    import
+      (
+        let
+          lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+        in
+        fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/${lock.nodes.nixpkgs.locked.rev}.tar.gz";
+          sha256 = lock.nodes.nixpkgs.locked.narHash;
+        }
+      )
+      { };
+  emanote = import ./.;
+in
+{ name ? "sridca/emanote"
+, tag ? "dev"
+}: pkgs.dockerTools.buildImage {
+  inherit name tag;
+  contents = [
+    emanote
+    # These are required for the GitLab CI runner
+    pkgs.coreutils
+    pkgs.bash_5
+  ];
+
+  config = {
+    WorkingDir = "/data";
+    Volumes = {
+      "/data" = { };
+    };
+  };
+}
