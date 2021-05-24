76c4adedede07390c8da024494dd6b6f4a5ab580
Sridhar Ratnakumar <srid@srid.ca>
2021-05-24 15:16:56 -0400

    Avoid hardcoding "./templates". Let the user choose path.
    Load default templates once, but don't reload again.
    
    TODO: When the user deletes an overriden default template, we should restore the default.

--- src/Emanote/Source.hs
+++ src/Emanote/Source.hs
@@ -8,23 +8,25 @@
 module Emanote.Source where
 
 import Control.Exception (throw)
-import Control.Lens.Operators ((%~), (.~))
+import Control.Lens.Operators ((%~))
 import Control.Monad.Logger
 import qualified Data.Aeson as Aeson
 import qualified Data.Set as Set
+import qualified Data.Text as Text
 import qualified Data.Yaml as Yaml
 import qualified Ema.Helper.FileSystem as FileSystem
 import qualified Ema.Helper.Markdown as Markdown
 import Emanote.Model (Model)
 import qualified Emanote.Model as M
 import qualified Emanote.Route as R
-import Emanote.Route.Ext
+import Emanote.Route.Ext (FileType (LMLType), LML (Md))
 import qualified Emanote.Route.Ext as Ext
 import qualified Heist.Extra.TemplateState as T
 import qualified Paths_emanote
-import System.Directory (doesDirectoryExist)
+import System.Directory (withCurrentDirectory)
 import System.FilePath ((</>))
 import System.FilePattern (FilePattern)
+import qualified System.FilePattern.Directory as FP
 
 log :: MonadLogger m => Text -> m ()
 log = logInfoNS "emanote"
@@ -42,7 +44,7 @@ data Source
   | -- | YAML data file
     SourceData
   | -- | Heist template file
-    SourceTemplate FilePath
+    SourceTemplate
   | -- | The rest are considered static files to copy as-is
     SourceStatic
   deriving (Eq, Ord, Show)
@@ -55,9 +57,9 @@ sourcePattern = \case
   SourceData ->
     Ext.withExt @'Ext.Yaml $
       "**/*"
-  SourceTemplate dir ->
+  SourceTemplate ->
     Ext.withExt @'Ext.HeistTpl $
-      dir </> "**/*"
+      "**/*"
   SourceStatic ->
     "**"
 
@@ -66,7 +68,7 @@ filePatterns =
   (id &&& sourcePattern)
     <$> [ SourceLML Ext.Md,
           SourceData,
-          SourceTemplate "templates",
+          SourceTemplate,
           SourceStatic
         ]
 
@@ -78,10 +80,19 @@ ignorePatterns =
 
 defaultTemplateState :: (MonadIO m, MonadLogger m) => m T.TemplateState
 defaultTemplateState = do
+  st <- T.newTemplateState
   defaultFiles <- liftIO Paths_emanote.getDataDir
   let tmplDir = defaultFiles </> "templates"
   log $ "Loading default templates from " <> toText tmplDir
-  T.loadHeistTemplates tmplDir
+  liftIO $
+    withCurrentDirectory defaultFiles $ do
+      files <- FP.getDirectoryFiles "." ["**/*.tpl"]
+      filesWithContent <- forM files $ \fp -> (fp,) <$> readFileBS fp
+      pure $ addTemplateFiles filesWithContent st
+
+addTemplateFiles :: [(FilePath, ByteString)] -> T.TemplateState -> T.TemplateState
+addTemplateFiles tpls ts = do
+  foldl' (\st (fp, s) -> T.addTemplateFile fp s st) ts tpls
 
 defaultData :: (MonadIO m, MonadLogger m) => m Aeson.Value
 defaultData = do
@@ -106,7 +117,7 @@ transformAction src fps action =
           fmap (fromMaybe id) . runMaybeT $ do
             r :: R.Route ('LMLType 'Md) <- MaybeT $ pure $ R.mkRouteFromFilePath @('Ext.LMLType 'Ext.Md) fp
             -- TODO: Log in batches, to avoid slowing things down when using large notebooks
-            logD $ "Reading note " <> toText fp
+            logD $ "Reading note: " <> toText fp
             !s <- readFileText fp
             (mMeta, doc) <- either (throw . BadInput) pure $ parseMarkdown fp s
             pure $ M.modelInsertMarkdown r (fromMaybe Aeson.Null mMeta, doc)
@@ -118,24 +129,20 @@ transformAction src fps action =
         chainM fps $ \fp ->
           fmap (fromMaybe id) . runMaybeT $ do
             r :: R.Route 'Ext.Yaml <- MaybeT $ pure $ R.mkRouteFromFilePath @'Ext.Yaml fp
-            logD $ "Reading data " <> toText fp
+            logD $ "Reading data: " <> toText fp
             !s <- readFileBS fp
             sdata <- parseSData s
             pure $ M.modelInsertData r sdata
       FileSystem.Delete ->
         chainM fps $ \fp ->
           pure $ maybe id M.modelDeleteData (R.mkRouteFromFilePath @'Ext.Yaml fp)
-    SourceTemplate dir -> do
+    SourceTemplate -> do
+      -- TODO: Juse use chainM?
       print fps
-      liftIO (doesDirectoryExist dir) >>= \case
-        True -> do
-          log "Reloading user templates"
-          tplState <- T.loadHeistTemplates dir
-          log $ show tplState
-          pure $ M.modelHeistTemplate .~ tplState
-        False -> do
-          -- Revert to default templates (the user has deleted theirs)
-          (M.modelHeistTemplate .~) <$> defaultTemplateState
+      tpls <- forM fps $ \fp -> (fp,) <$> readFileBS fp
+      -- TODO: Handle *removing* of templates! ... however, don't remove *default* ones.
+      log $ "Reading " <> show (length tpls) <> " template: " <> Text.intercalate ", " (toText . fst <$> tpls)
+      pure $ M.modelHeistTemplate %~ addTemplateFiles tpls
     SourceStatic -> do
       let setAction = case action of
             FileSystem.Update -> Set.union
