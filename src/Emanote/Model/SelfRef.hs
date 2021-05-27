e1fcbeb2b148ce1a56b5d02e8229f85d18a9f5cd
Sridhar Ratnakumar <srid@srid.ca>
2021-05-27 18:50:29 -0400

    Allow stylying broken links
    Add a default style, that uses line-through and italic.
    
    Fixes #40

--- src/Emanote/Model/SelfRef.hs
+++ src/Emanote/Model/SelfRef.hs
@@ -3,7 +3,8 @@ module Emanote.Model.SelfRef where
 import Emanote.Route.SomeRoute (SomeRoute)
 import qualified Emanote.Route.WikiLink as WL
 
--- | Any potential WikiLink that refer to a note.
+-- | Any potential WikiLink that refer to something
+-- TODO: Should ditch this and use WikiLink directly?
 newtype SelfRef = SelfRef {unSelfRef :: WL.WikiLink}
   deriving (Eq, Ord, Show)
 
