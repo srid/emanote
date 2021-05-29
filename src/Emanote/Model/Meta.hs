fc48c27159ef29d4e27aa882ed5fa9e9069e1379
Sridhar Ratnakumar <srid@srid.ca>
2021-05-29 18:25:54 -0400

    Add tag index to IxNote

--- src/Emanote/Model/Meta.hs
+++ src/Emanote/Model/Meta.hs
@@ -12,31 +12,15 @@ import Data.Aeson (FromJSON)
 import qualified Data.Aeson as Aeson
 import qualified Data.IxSet.Typed as Ix
 import Emanote.Model (Model, modelLookupNote, modelSData)
-import Emanote.Model.Note (noteMeta)
+import Emanote.Model.Note (lookupAeson, noteMeta)
 import Emanote.Model.SData (sdataValue)
 import qualified Emanote.Model.SData as SData
 import qualified Emanote.Route as R
-import Relude.Extra.Map (StaticMap (lookup))
 
 -- | Look up a specific key in the meta for a given route.
 lookupMeta :: FromJSON a => a -> NonEmpty Text -> R.LinkableLMLRoute -> Model -> a
 lookupMeta x k r =
-  lookupMetaFrom x k . getEffectiveRouteMeta r
-
--- TODO: Use https://hackage.haskell.org/package/lens-aeson
-lookupMetaFrom :: forall a. FromJSON a => a -> NonEmpty Text -> Aeson.Value -> a
-lookupMetaFrom x (k :| ks) meta =
-  fromMaybe x $ do
-    Aeson.Object obj <- pure meta
-    val <- lookup k obj
-    case nonEmpty ks of
-      Nothing -> resultToMaybe $ Aeson.fromJSON val
-      Just ks' -> pure $ lookupMetaFrom x ks' val
-  where
-    resultToMaybe :: Aeson.Result b -> Maybe b
-    resultToMaybe = \case
-      Aeson.Error _ -> Nothing
-      Aeson.Success b -> pure b
+  lookupAeson x k . getEffectiveRouteMeta r
 
 -- | Get the (final) metadata of a note at the given route, by merging it with
 -- the defaults specified in parent routes all the way upto index.yaml.
