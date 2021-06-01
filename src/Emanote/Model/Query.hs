c1dd26a4c3c01e14fcd0d43232e8edd05b111a78
Sridhar Ratnakumar <srid@srid.ca>
2021-06-01 17:18:26 -0400

    Allow styling query results N>1 number of ways
    Depending on the class set.
    
    Add a 'timeline' query for including listing with date.

--- src/Emanote/Model/Query.hs
+++ src/Emanote/Model/Query.hs
@@ -1,11 +1,11 @@
 module Emanote.Model.Query where
 
-import Control.Lens.Operators
+import Control.Lens.Operators ((^.))
 import Data.IxSet.Typed ((@=))
 import qualified Data.IxSet.Typed as Ix
 import qualified Data.Text as T
-import Emanote.Model.Note
-import Emanote.Model.Type
+import Emanote.Model.Note (Note)
+import Emanote.Model.Type (Model, modelNotes)
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as M
 import qualified Text.Show as Show
@@ -17,7 +17,7 @@ data Query
 instance Show.Show Query where
   show = \case
     QueryByTag tag ->
-      toString $ "Notes tagged #" <> tag
+      toString $ "Pages tagged #" <> tag
 
 parseQuery :: Text -> Maybe Query
 parseQuery = do
