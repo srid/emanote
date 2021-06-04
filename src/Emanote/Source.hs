da1a0f90a779604b462726f600045fc3b1e5a66a
Sridhar Ratnakumar <srid@srid.ca>
2021-06-04 12:40:33 -0400

    Encapsulate Emanote.Source

--- src/Emanote/Source.hs
+++ src/Emanote/Source.hs
@@ -1,9 +1,28 @@
 {-# LANGUAGE TypeApplications #-}
 
+-- | Emanote.Source is responsible for managing the source files and its
+-- changes, and communicating them to `Emanote.Model`.
 module Emanote.Source
-  ( module X,
+  ( run,
   )
 where
 
-import Emanote.Source.Loc as X
-import Emanote.Source.Patch as X
+import Control.Monad.Logger (MonadLogger)
+import Data.LVar (LVar)
+import Emanote.Model (Model)
+import Emanote.Source.Loc (locLayers)
+import qualified Emanote.Source.Mount as Mount
+import Emanote.Source.Patch (transformActions)
+import Emanote.Source.Pattern (filePatterns, ignorePatterns)
+import UnliftIO (MonadUnliftIO)
+
+run :: (MonadUnliftIO m, MonadLogger m) => LVar Model -> Model -> m ()
+run modelLvar initialModel = do
+  fsLayers <- liftIO locLayers
+  Mount.unionMountOnLVar
+    fsLayers
+    filePatterns
+    ignorePatterns
+    modelLvar
+    initialModel
+    transformActions
