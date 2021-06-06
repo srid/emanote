c3cb6eccdca720a8d5364c99ba054e44c2fc853a
Sridhar Ratnakumar <srid@srid.ca>
2021-06-06 13:20:05 -0400

    Style inline tag

--- src/Emanote/Pandoc/Filter/Url.hs
+++ src/Emanote/Pandoc/Filter/Url.hs
@@ -12,7 +12,7 @@ import qualified Emanote.Model.Link.Rel as Rel
 import qualified Emanote.Model.Note as MN
 import qualified Emanote.Model.StaticFile as SF
 import qualified Emanote.Route as R
-import Emanote.View.SiteRoute (SiteRoute (SRIndex, SRLMLFile, SRStaticFile))
+import Emanote.View.SiteRoute (SiteRoute (..))
 import qualified Emanote.View.SiteRoute as SR
 import qualified Heist.Extra.Splices.Pandoc as HP
 import qualified Heist.Interpreted as HI
