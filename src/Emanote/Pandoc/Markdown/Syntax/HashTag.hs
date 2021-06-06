5e1c32f2bbf27740d5277d397b66740aace975a4
Sridhar Ratnakumar <srid@srid.ca>
2021-06-06 14:18:05 -0400

    Tag query to pull in inline tags

--- src/Emanote/Pandoc/Markdown/Syntax/HashTag.hs
+++ src/Emanote/Pandoc/Markdown/Syntax/HashTag.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 -- | TODO: Rename to HashTag?
 module Emanote.Pandoc.Markdown.Syntax.HashTag
   ( hashTagSpec,
-    hashTags,
+    inlineTagsInPandoc,
     HashTag (..),
   )
 where
@@ -14,30 +16,36 @@ import qualified Commonmark as CM
 import qualified Commonmark.Inlines as CM
 import qualified Commonmark.Pandoc as CP
 import Commonmark.TokParsers (noneOfToks, symbol)
-import Ema.Helper.Markdown (plainify)
+import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Map.Strict as Map
 import qualified Text.Pandoc.Builder as B
 import qualified Text.Pandoc.Walk as W
 import qualified Text.Parsec as P
 
 mkHashTagFrom :: B.Inline -> Maybe HashTag
 mkHashTagFrom = \case
-  B.Span (_, [cls], _) is
-    | cls == hashTagCls ->
-      pure $ HashTag $ plainify is
+  B.Span (_, [cls], Map.fromList -> attrs) _
+    | cls == hashTagCls -> do
+      tag <- Map.lookup "data" attrs
+      pure $ HashTag tag
   _ -> Nothing
 
-hashTags :: B.Pandoc -> [HashTag]
-hashTags = W.query $ maybeToList . mkHashTagFrom
+inlineTagsInPandoc :: B.Pandoc -> [HashTag]
+inlineTagsInPandoc = W.query $ maybeToList . mkHashTagFrom
 
 newtype HashTag = HashTag {unHashTag :: Text}
-  deriving (Eq, Show, Ord)
+  deriving (Eq, Show, Ord, Generic, ToJSON, FromJSON)
 
 class HasHashTag il where
   hashTag :: HashTag -> il
 
 instance HasHashTag (CP.Cm b B.Inlines) where
   hashTag (HashTag tag) =
-    CP.Cm $ B.spanWith ("", one hashTagCls, one ("title", "Tag")) $ B.str $ "#" <> tag
+    let attrs =
+          [ ("title", "Tag"),
+            ("data", tag)
+          ]
+     in CP.Cm $ B.spanWith ("", one hashTagCls, attrs) $ B.str $ "#" <> tag
 
 hashTagCls :: Text
 hashTagCls = "emanote:inline-tag"
