31f08893b7b48a888308194f9166f7f4ad63606a
Sridhar Ratnakumar <srid@srid.ca>
2021-06-10 14:55:54 -0400

    Remove special pandoc.tpl tags from output

--- src/Heist/Extra/Splices/Pandoc/Ctx.hs
+++ src/Heist/Extra/Splices/Pandoc/Ctx.hs
@@ -81,8 +81,14 @@ inlineLookupAttr node = \case
 -- | Useful for running a splice against an arbitrary node (such as that pulled from pandoc.tpl)
 runCustomNode :: Monad n => X.Node -> H.Splices (HI.Splice n) -> HI.Splice n
 runCustomNode node splices =
-  H.localHS (HI.bindSplices splices) $
-    HI.runNode node
+  H.localHS (HI.bindSplices splices) $ do
+    HI.runNode node <&> \case
+      [resNode]
+        | X.elementTag resNode == X.elementTag node ->
+          -- Get rid of the `node` itself.
+          X.elementChildren resNode
+      res ->
+        res
 
 childTagAttr :: X.Node -> Text -> B.Attr
 childTagAttr x name =
