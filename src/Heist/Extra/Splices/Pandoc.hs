e1e85a7f0ea2f240e5f439c02ac210bdc297ba59
Sridhar Ratnakumar <srid@srid.ca>
2021-06-10 13:54:18 -0400

    Actually use the footnote ref splice

--- src/Heist/Extra/Splices/Pandoc.hs
+++ src/Heist/Extra/Splices/Pandoc.hs
@@ -6,12 +6,9 @@
 module Heist.Extra.Splices.Pandoc
   ( RenderCtx (..),
     pandocSplice,
-    pandocSpliceWithCustomClass,
     -- | To delegate rendering of blocks and inlines from a custom splice.
     rpBlock,
     rpInline,
-    rpBlock',
-    rpInline',
   )
 where
 
@@ -20,22 +17,19 @@ import Heist.Extra.Splices.Pandoc.Ctx
   ( RenderCtx (..),
     mkRenderCtx,
   )
-import Heist.Extra.Splices.Pandoc.Footnotes (gatherFootnotes, renderFootnotesWith)
+import Heist.Extra.Splices.Pandoc.Footnotes (footnoteRefSplice, gatherFootnotes, renderFootnotesWith)
 import Heist.Extra.Splices.Pandoc.Render
+  ( renderPandocWith,
+    rpBlock,
+    rpInline,
+  )
 import qualified Heist.Interpreted as HI
 import qualified Text.Pandoc.Builder as B
 import Text.Pandoc.Definition (Pandoc (..))
 
-pandocSplice :: Monad n => Pandoc -> HI.Splice n
-pandocSplice =
-  pandocSpliceWithCustomClass
-    mempty
-    (const . const $ Nothing)
-    (const . const $ Nothing)
-
 -- | A splice to render a Pandoc AST allowing customization of the AST nodes in
 -- HTML.
-pandocSpliceWithCustomClass ::
+pandocSplice ::
   Monad n =>
   -- | How to replace classes in Div and Span nodes.
   Map Text Text ->
@@ -45,15 +39,30 @@ pandocSpliceWithCustomClass ::
   (RenderCtx n -> B.Inline -> Maybe (HI.Splice n)) ->
   Pandoc ->
   HI.Splice n
-pandocSpliceWithCustomClass classMap bS iS doc = do
+pandocSplice classMap bS iS doc = do
   node <- H.getParamNode
-  let ctx =
+  -- Render main document, sans footnote content.
+  let footnotes = gatherFootnotes doc
+      iSWithFootnotes c i =
+        asum
+          [ iS c i,
+            footnoteRefSplice c i
+          ]
+      ctx =
+        mkRenderCtx
+          node
+          classMap
+          bS
+          iSWithFootnotes
+          footnotes
+  docNodes <- renderPandocWith ctx doc
+  -- Render footnotes themselves, but without recursing into inner footnotes.
+  let footnotesCtx =
         mkRenderCtx
           node
           classMap
           bS
           iS
-          (gatherFootnotes doc)
-  docNodes <- renderPandocWith ctx doc
-  footnotesNodes <- renderFootnotesWith (ctx {footnotes = []}) $ footnotes ctx
+          []
+  footnotesNodes <- renderFootnotesWith footnotesCtx footnotes
   pure $ docNodes <> footnotesNodes
