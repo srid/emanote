d8046d99b334904123080622cc9e43f3f89371dd
Sridhar Ratnakumar <srid@srid.ca>
2021-06-17 22:52:16 -0400

    Finalize and docs

--- src/Emanote/View/Template.hs
+++ src/Emanote/View/Template.hs
@@ -33,6 +33,7 @@ import qualified Emanote.Route.SiteRoute as SR
 import qualified Heist as H
 import qualified Heist.Extra.Splices.List as Splices
 import qualified Heist.Extra.Splices.Pandoc as Splices
+import Heist.Extra.Splices.Pandoc.Render (withoutH1)
 import qualified Heist.Extra.Splices.Tree as Splices
 import qualified Heist.Extra.TemplateState as Tmpl
 import qualified Heist.Interpreted as HI
@@ -145,12 +146,6 @@ renderLmlHtml emaAction model note = do
         (PF.urlResolvingSplice emaAction model)
       $ note ^. MN.noteDoc
         & withoutH1 -- Because, handling note title separately
-  where
-    withoutH1 :: Pandoc -> Pandoc
-    withoutH1 (Pandoc meta (B.Header 1 _ _ : rest)) =
-      Pandoc meta rest
-    withoutH1 doc =
-      doc
 
 commonSplices :: Monad n => Ema.CLI.Action -> Aeson.Value -> Text -> H.Splices (HI.Splice n)
 commonSplices emaAction meta routeTitle = do
