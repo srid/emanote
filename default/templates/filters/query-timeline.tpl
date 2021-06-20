3e49cea305e0afe00a25b38b913a2a41dce347fd
Sridhar Ratnakumar <srid@srid.ca>
2021-06-20 13:02:15 -0400

    Define query tpl consistently with embed tpl

---
+++ default/templates/filters/query-timeline.tpl
@@ -0,0 +1,20 @@
+<!-- TODO: DRY (same as query-default.tpl, except for date column)-->
+<nav>
+  <!-- This should be a grid -->
+  <div class="grid grid-cols-12 gap-2 mt-4">
+    <span class="col-span-3"></span>
+    <header class="col-span-9 pb-2 mb-2 font-semibold text-gray-600 border-b-2">
+      <query />
+    </header>
+    <result>
+      <ema:note:metadata>
+        <span class="col-span-3 pr-2 mr-2 text-right text-gray-600 border-r-2">
+          <value var="date" />
+        </span>
+      </ema:note:metadata>
+      <a class="col-span-9 text-${theme}-600 font-bold hover:underline" href="${ema:note:url}">
+        <ema:note:title />
+      </a>
+    </result>
+  </div>
+</nav>
--- src/Emanote/Pandoc/Filter/Query.hs
+++ src/Emanote/Pandoc/Filter/Query.hs
@@ -4,7 +4,6 @@ module Emanote.Pandoc.Filter.Query (queryResolvingSplice, noteSplice) where
 
 import Control.Lens.Operators ((^.))
 import qualified Data.List as List
-import qualified Data.List.NonEmpty as NE
 import Data.Map.Syntax ((##))
 import qualified Data.Text as T
 import qualified Ema
@@ -13,37 +12,32 @@ import qualified Emanote.Model.Note as MN
 import qualified Emanote.Model.Query as Q
 import qualified Emanote.Route.SiteRoute as SR
 import qualified Heist as H
-import qualified Heist.Extra as H
+import qualified Heist.Extra as HE
 import qualified Heist.Extra.Splices.Pandoc as HP
 import qualified Heist.Interpreted as HI
 import qualified Heist.Splices.Json as HJ
 import qualified Text.Pandoc.Definition as B
-import qualified Text.XmlHtml as X
 
 queryResolvingSplice :: Monad n => MN.Note -> Model -> HP.RenderCtx n -> B.Block -> Maybe (HI.Splice n)
-queryResolvingSplice currentNote model HP.RenderCtx {..} blk = do
+queryResolvingSplice currentNote model _ctx blk = do
   B.CodeBlock
     (_id', classes, _attrs)
     (Q.parseQuery -> Just q) <-
     pure blk
   guard $ List.elem "query" classes
   let mOtherCls = nonEmpty (List.delete "query" classes) <&> T.intercalate " " . toList
-  queryNode <- childElementTagWithClass "CodeBlock:Query" mOtherCls rootNode
-  pure $
-    H.runCustomNode queryNode $ do
+      queryTpl = encodeUtf8 $ "/templates/filters/query-" <> fromMaybe "default" mOtherCls
+  pure $ do
+    tpl <- HE.lookupHtmlTemplateMust queryTpl
+    HE.runCustomTemplate tpl $ do
       "query"
         ## HI.textSplice (show q)
       "result"
         ## (HI.runChildrenWith . noteSplice model) `foldMapM` Q.runQuery currentNote model q
-  where
-    childElementTagWithClass tag mCls node = do
-      queryNodes <- nonEmpty $ X.childElementsTag tag node
-      fmap head . nonEmpty $
-        NE.filter ((== mCls) . X.getAttribute "class") queryNodes
 
 -- TODO: Reuse this elsewhere
 noteSplice :: Monad n => Model -> MN.Note -> H.Splices (HI.Splice n)
 noteSplice model note = do
-  "note:title" ## HI.textSplice (MN.noteTitle note)
-  "note:url" ## HI.textSplice (Ema.routeUrl model $ SR.lmlSiteRoute $ note ^. MN.noteRoute)
-  "note:metadata" ## HJ.bindJson (note ^. MN.noteMeta)
+  "ema:note:title" ## HI.textSplice (MN.noteTitle note)
+  "ema:note:url" ## HI.textSplice (Ema.routeUrl model $ SR.lmlSiteRoute $ note ^. MN.noteRoute)
+  "ema:note:metadata" ## HJ.bindJson (note ^. MN.noteMeta)
