8140d1a47839063d36ad745cac233c2a298a9b24
Sridhar Ratnakumar <srid@srid.ca>
2021-06-28 11:29:17 -0400

    WIP: Add --layers CLI option

---
+++ src/Emanote/CLI.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Emanote.CLI (
+  Cli(..),
+  parseCli
+) where
+
+import qualified Ema.CLI
+import Options.Applicative hiding (action)
+import qualified Paths_emanote
+import Data.Version (showVersion)
+import qualified Data.Text as T
+import UnliftIO.Directory (getCurrentDirectory)
+
+data Cli = Cli
+  { layers :: NonEmpty FilePath,
+    emaCli :: Ema.CLI.Cli
+  }
+
+cliParser :: FilePath -> Parser Cli
+cliParser cwd = do
+  layers <- pathList (one cwd)
+  emaCli <- Ema.CLI.cliParser
+  pure Cli {..}
+  where
+    pathList defaultPath = do
+      option pathListReader $
+        mconcat
+          [ long "layers",
+            metavar "LAYERS",
+            value defaultPath,
+            help "List of notebook folders to 'union mount', with the right-side folders taking priority."
+          ]
+    pathListReader :: ReadM (NonEmpty FilePath)
+    pathListReader = 
+      maybeReader $ \paths ->
+        nonEmpty $ fmap toString $ T.split (== ';') . toText $ paths 
+
+parseCli' :: FilePath -> ParserInfo Cli
+parseCli' cwd =
+  info
+    (versionOption <*> cliParser cwd <**> helper)
+    ( fullDesc
+        <> progDesc "Emanote - A spiritual successor to Neuron"
+        <> header "Emanote"
+    )
+  where
+    versionOption =
+      infoOption
+        (showVersion Paths_emanote.version)
+        (long "version" <> help "Show version")
+
+parseCli :: IO Cli
+parseCli = 
+  execParser . parseCli' =<< getCurrentDirectory 
