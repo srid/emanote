bde7b2b4be23f65dd42c84107ff7ff8a5209b9ac
Sridhar Ratnakumar <srid@srid.ca>
2021-06-28 11:29:17 -0400

    WIP: Add --layers CLI option

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,11 @@ import qualified Emanote.Model as Model
 import qualified Emanote.Source as Source
 import qualified Emanote.View as View
 import Main.Utf8 (withUtf8)
+import qualified Emanote.CLI as CLI
 
 main :: IO ()
 main =
-  withUtf8 $
-    Ema.runEma View.render $ \_act m ->
+  withUtf8 $ do 
+    cli <- CLI.parseCli
+    Ema.runEmaWithCli (CLI.emaCli cli) View.render $ \_act m ->
       Source.emanate m =<< Model.emptyModel
