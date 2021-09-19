dd751727a826b93aafb968da35aaf967d65bff70
Sridhar Ratnakumar <srid@srid.ca>
2021-09-19 12:33:20 -0400

    stash static binary work

--- flake.nix
+++ flake.nix
@@ -29,8 +29,20 @@
         overlays = [ ];
         pkgs =
           import nixpkgs { inherit system overlays; config.allowBroken = true; };
+        pkgsStatic =
+          pkgs.pkgsMusl;
         windicss =
           (import inputs.windicss { inherit pkgs; }).shell.nodeDependencies;
+        neuronFlags = [
+          "--ghc-option=-optl=-static"
+          # Disabling shared as workaround. But - https://github.com/nh2/static-haskell-nix/issues/99#issuecomment-665400600
+          # TODO: Patch ghc bootstrap binary to use ncurses6, which might also obviate the nixpkgs revert.
+          "--disable-shared"
+          "--extra-lib-dirs=${pkgsStatic.gmp6.override { withStatic = true; }}/lib"
+          "--extra-lib-dirs=${pkgsStatic.zlib.static}/lib"
+          "--extra-lib-dirs=${pkgsStatic.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+          "--extra-lib-dirs=${pkgsStatic.ncurses.override { enableStatic = true; }}/lib"
+        ];
         # Based on https://github.com/input-output-hk/daedalus/blob/develop/yarn2nix.nix#L58-L71
         filter = name: type:
           let
@@ -46,12 +58,12 @@
               sansPrefix == "/.vscode"
             );
         project = returnShellEnv:
-          pkgs.haskellPackages.developPackage {
+          pkgsStatic.haskellPackages.developPackage {
             inherit returnShellEnv;
             name = "emanote";
-            root = pkgs.lib.cleanSourceWith { inherit filter; src = ./.; name = "emanote"; };
+            root = pkgsStatic.lib.cleanSourceWith { inherit filter; src = ./.; name = "emanote"; };
             withHoogle = true;
-            overrides = self: super: with pkgs.haskell.lib; {
+            overrides = self: super: with pkgsStatic.haskell.lib; {
               ema = disableCabalFlag inputs.ema.defaultPackage.${system} "with-examples";
               tagtree = self.callCabal2nix "tagtree" inputs.tagtree { };
               # Jailbreak heist to allow newer dlist
@@ -74,7 +86,7 @@
       in
       {
         # Used by `nix build` & `nix run`
-        defaultPackage = project false;
+        defaultPackage = pkgs.haskell.lib.appendConfigureFlags (project false) neuronFlags;
 
         inherit windicss;
 
