c5c4a3f61ce056fd64a4a8cc3f1d860dd58f95a5
Sridhar Ratnakumar <srid@srid.ca>
2021-09-23 21:41:21 -0400

    Avoid crashing on #148 (use broken URL in place)

--- src/Emanote/Route/SiteRoute/Class.hs
+++ src/Emanote/Route/SiteRoute/Class.hs
@@ -85,10 +85,9 @@ encodeResourceRoute :: HasCallStack => Model -> ResourceRoute -> FilePath
 encodeResourceRoute model =
   absurdUnion
     `h` ( \(r :: LMLRoute) ->
-            R.encodeRoute $
-              -- HACK: This should never fail.
-              maybe (error $ "attempt to encode a non-existance note route: " <> show r) N.noteHtmlRoute $
-                M.modelLookupNoteByRoute r model
+            -- HACK: This should never fail, but it does; see https://github.com/srid/emanote/issues/148
+            maybe "/-/emanote/bug/148" (R.encodeRoute . N.noteHtmlRoute) $
+              M.modelLookupNoteByRoute r model
         )
     `h` ( \(r :: StaticFileRoute, _fpAbs :: FilePath) ->
             R.encodeRoute r
