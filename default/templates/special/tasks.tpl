12bbd2bc30850bc114d4aa4c6467c4f14fbedd22
Sridhar Ratnakumar <srid@srid.ca>
2021-10-22 12:01:31 -0400

    Connect them all (view, template, model)

--- default/templates/special/tasks.tpl
+++ default/templates/special/tasks.tpl
@@ -1,6 +1,20 @@
 <!-- TODO: Parametrize experimental thingy -->
 <apply template="/templates/special/base">
   <bind tag="special-main">
-    Tasks Tracker
+    Tasks as follows:
+    <ul>
+      <ema:tasks>
+        <task>
+          <li>
+            <task:description />
+            (
+            <a class="text-${theme}-600 font-bold hover:bg-${theme}-50" href="${note:url}">
+              <note:title />
+            </a>
+            )
+          </li>
+        </task>
+      </ema:tasks>
+    </ul>
   </bind>
 </apply>
--- src/Emanote/View/Tasks.hs
+++ src/Emanote/View/Tasks.hs
@@ -2,14 +2,45 @@
 
 module Emanote.View.Tasks (renderTasks) where
 
+import Control.Lens.Operators ((^.))
+import qualified Data.IxSet.Typed as Ix
+import Data.Map.Syntax ((##))
 import Emanote.Model (Model)
 import qualified Emanote.Model.Meta as Meta
-import Emanote.View.Common (commonSplices, renderModelTemplate)
+import qualified Emanote.Model.Task as Task
+import qualified Emanote.Model.Title as Title
+import qualified Emanote.Model.Type as M
+import Emanote.Pandoc.BuiltinFilters (preparePandoc)
+import qualified Emanote.Route.SiteRoute as SR
+import Emanote.View.Common (commonSplices, inlineRenderers, linkInlineRenderers, mkRendererFromMeta, renderModelTemplate)
+import qualified Heist.Extra.Splices.List as Splices
+import qualified Heist.Extra.Splices.Pandoc as Splices
 import Heist.Extra.Splices.Pandoc.Ctx (emptyRenderCtx)
+import qualified Heist.Interpreted as HI
 import Relude
+import qualified Text.Pandoc.Definition as B
 
 renderTasks :: Model -> LByteString
 renderTasks model = do
   let meta = Meta.getIndexYamlMeta model
+      -- TODO: Refactor with Template.hs for DRY
+      withNoteRenderer = mkRendererFromMeta model meta
+      withInlineCtx =
+        withNoteRenderer inlineRenderers () ()
+      withLinkInlineCtx =
+        withNoteRenderer linkInlineRenderers () ()
+      titleSplice titleDoc = withLinkInlineCtx $ \x ->
+        Title.titleSplice x (preparePandoc model) titleDoc
+      tasks = Ix.toList $ model ^. M.modelTasks
+      taskSplice task = do
+        let r = task ^. Task.taskRoute
+        -- TODO: reuse note splice
+        "task:description" ## withInlineCtx $ \ctx ->
+          Splices.pandocSplice ctx $ B.Pandoc mempty $ task ^. Task.taskDescription
+        "note:title" ## titleSplice (M.modelLookupTitle r model)
+        "note:url" ## HI.textSplice (SR.siteRouteUrl model $ SR.lmlSiteRoute r)
+
   renderModelTemplate model "templates/special/tasks" $ do
     commonSplices ($ emptyRenderCtx) model meta "Task Tracker"
+    "ema:tasks"
+      ## Splices.listSplice tasks "task" taskSplice
