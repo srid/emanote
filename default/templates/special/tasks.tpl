710b81e004bb4d49451bb7748bb40d591822258b
Sridhar Ratnakumar <srid@srid.ca>
2021-10-23 12:06:44 -0400

    Group tasks by containing note

--- default/templates/special/tasks.tpl
+++ default/templates/special/tasks.tpl
@@ -1,20 +1,44 @@
 <!-- TODO: Parametrize experimental thingy -->
 <apply template="/templates/special/base">
   <bind tag="special-main">
-    Tasks as follows:
-    <ul>
-      <ema:tasks>
-        <task>
-          <li>
-            <task:description />
-            (
-            <a class="text-${theme}-600 font-bold hover:bg-${theme}-50" href="${note:url}">
-              <note:title />
-            </a>
-            )
-          </li>
-        </task>
-      </ema:tasks>
-    </ul>
+    <div class="w-full">
+      <div class="flex flex-col items-center justify-left">
+        <div class="w-full px-3 py-3 bg-gray-200">
+          <p>All tasks indexed by their containing note</p>
+        </div>
+        <div class="w-full bg-white px-3">
+          <ema:taskGroups>
+            <taskGroup>
+              <section class="my-4">
+                <div>
+                  <a class="text-${theme}-600 font-bold hover:bg-${theme}-50" href="${t:note:url}">
+                    <t:note:title />
+                  </a>
+                </div>
+                <t:tasks>
+                  <task>
+                    <div>
+                      <label class="inline-flex items-start space-x-2">
+                        <input type="checkbox" class="flex-shrink-0 w-5 h-5">
+                        <span>
+                          <task:description>
+                            <PandocLink class="text-gray-600">
+                              <Internal class="font-bold hover:bg-gray-50" />
+                              <External class="hover:underline" target="_blank" rel="noopener" />
+                            </PandocLink>
+                            <OrderedList class="ml-4 space-y-1 list-decimal list-inside" />
+                            <BulletList class="ml-4 space-y-1 list-decimal list-inside" />
+                          </task:description>
+                        </span>
+                      </label>
+                    </div>
+                  </task>
+                </t:tasks>
+              </section>
+            </taskGroup>
+          </ema:taskGroups>
+        </div>
+      </div>
+    </div>
   </bind>
 </apply>
--- src/Emanote/View/TaskIndex.hs
+++ src/Emanote/View/TaskIndex.hs
@@ -4,13 +4,16 @@ module Emanote.View.TaskIndex (renderTasks) where
 
 import Control.Lens.Operators ((^.))
 import qualified Data.IxSet.Typed as Ix
+import qualified Data.Map.Strict as Map
 import Data.Map.Syntax ((##))
 import Emanote.Model (Model)
 import qualified Emanote.Model.Meta as Meta
+import Emanote.Model.Task (Task)
 import qualified Emanote.Model.Task as Task
 import qualified Emanote.Model.Title as Title
 import qualified Emanote.Model.Type as M
 import Emanote.Pandoc.BuiltinFilters (preparePandoc)
+import qualified Emanote.Route as R
 import qualified Emanote.Route.SiteRoute as SR
 import Emanote.View.Common (commonSplices, inlineRenderers, linkInlineRenderers, mkRendererFromMeta, renderModelTemplate)
 import qualified Heist.Extra.Splices.List as Splices
@@ -19,6 +22,14 @@ import Heist.Extra.Splices.Pandoc.Ctx (emptyRenderCtx)
 import qualified Heist.Interpreted as HI
 import Relude
 import qualified Text.Pandoc.Definition as B
+
+newtype TaskIndex = TaskIndex {unTaskIndex :: Map R.LMLRoute (NonEmpty Task)}
+
+mkTaskIndex :: Model -> TaskIndex
+mkTaskIndex model =
+  TaskIndex . Map.fromListWith (<>) $
+    filter (not . Task._taskChecked) (Ix.toList $ model ^. M.modelTasks) <&> \task ->
+      (task ^. Task.taskRoute, one task)
 
 renderTasks :: Model -> LByteString
 renderTasks model = do
@@ -31,7 +42,11 @@ renderTasks model = do
         withNoteRenderer linkInlineRenderers () ()
       titleSplice titleDoc = withLinkInlineCtx $ \x ->
         Title.titleSplice x (preparePandoc model) titleDoc
-      tasks = Ix.toList $ model ^. M.modelTasks
+      taskIndex = mkTaskIndex model
+      taskGroupSplice r tasks = do
+        "t:note:url" ## HI.textSplice (SR.siteRouteUrl model $ SR.lmlSiteRoute r)
+        "t:note:title" ## titleSplice (M.modelLookupTitle r model)
+        "t:tasks" ## Splices.listSplice (toList tasks) "task" taskSplice
       taskSplice task = do
         let r = task ^. Task.taskRoute
         -- TODO: reuse note splice
@@ -41,7 +56,8 @@ renderTasks model = do
         "note:url" ## HI.textSplice (SR.siteRouteUrl model $ SR.lmlSiteRoute r)
 
   renderModelTemplate model "templates/special/tasks" $ do
-    commonSplices ($ emptyRenderCtx) model meta "Task Tracker"
-    let uncheckedTasks = filter (not . Task._taskChecked) tasks
-    "ema:tasks"
-      ## Splices.listSplice uncheckedTasks "task" taskSplice
+    commonSplices ($ emptyRenderCtx) model meta "Task Index"
+    let groups =
+          Map.toList (unTaskIndex taskIndex)
+            & sortOn fst
+    "ema:taskGroups" ## Splices.listSplice groups "taskGroup" (uncurry taskGroupSplice)
