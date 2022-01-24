e9f6e884ff687f7df281da2586592a10b389f412
Sridhar Ratnakumar <srid@srid.ca>
2022-01-24 14:22:24 -0500

    Add aarch64-linux to flake.nix
    https://github.com/srid/emanote/discussions/239

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
     };
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ]
       (system:
         let
           overlays = [ ];
