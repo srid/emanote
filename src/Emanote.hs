9fa23636e974914601c1d6d3ffea061f05714aa4
Sridhar Ratnakumar <srid@srid.ca>
2022-02-23 19:02:13 -0500

    Expose mkEmanoteSite

--- src/Emanote.hs
+++ src/Emanote.hs
@@ -1,20 +1,55 @@
 module Emanote
-  ( emanate,
+  ( mkEmanoteSite,
+    emanate,
     ChangeHandler,
   )
 where
 
 import Control.Monad.Logger (MonadLogger)
 import Data.LVar (LVar)
-import Data.LVar qualified as LVar
 import Data.Map.Strict qualified as Map
-import Emanote.Prelude (chainM, log)
+import Data.UUID.V4 qualified as UUID
+import Ema
+import Emanote.CLI qualified as CLI
+import Emanote.Model.Type qualified as Model
+import Emanote.Prelude (chainM)
+import Emanote.Route.SiteRoute.Class (routeEncoder)
+import Emanote.Route.SiteRoute.Type (SiteRoute)
 import Emanote.Source.Loc (Loc)
+import Emanote.Source.Loc qualified as Loc
+import Emanote.Source.Patch qualified as Patch
+import Emanote.Source.Pattern qualified as Pattern
+import Emanote.View qualified as View
+import Paths_emanote qualified
 import Relude
 import System.FilePattern (FilePattern)
 import System.UnionMount qualified as UM
 import UnliftIO (BufferMode (..), MonadUnliftIO, hSetBuffering)
 import UnliftIO.IO (hFlush)
+
+-- | Create an Emanote Ema site.
+mkEmanoteSite :: CLI.Cli -> Site Model.Model SiteRoute
+mkEmanoteSite cli =
+  Site
+    { siteName = "emanote",
+      siteRender = SiteRender $ \m r -> do
+        pure $ View.render m r,
+      siteRouteEncoder = routeEncoder,
+      siteModelManager =
+        ModelManager $ do
+          cliAct <- askCLIAction
+          enc <- askRouteEncoder
+          defaultLayer <- Loc.defaultLayer <$> liftIO Paths_emanote.getDataDir
+          instanceId <- liftIO UUID.nextRandom
+          let layers = one defaultLayer <> Loc.userLayers (CLI.layers cli)
+          lift $
+            Emanote.emanate
+              layers
+              Pattern.filePatterns
+              Pattern.ignorePatterns
+              (Model.emptyModel cliAct enc instanceId)
+              Patch.patchModel
+    }
 
 type ChangeHandler tag model m = tag -> FilePath -> UM.FileAction (NonEmpty (Loc, FilePath)) -> m (model -> model)
 
--- src/Main.hs
+++ src/Main.hs
@@ -3,23 +3,13 @@ module Main where
 import Control.Lens.Operators
 import Control.Monad.Logger (runStdoutLoggingT)
 import Data.Default (Default (def))
-import Data.Dependent.Sum (DSum ((:=>)))
 import Data.Some
-import Data.UUID.V4 qualified as UUID
 import Ema
-import Ema qualified
 import Ema.CLI qualified
-import Emanote qualified
+import Emanote (mkEmanoteSite)
 import Emanote.CLI qualified as CLI
-import Emanote.Model qualified as Model
-import Emanote.Route.SiteRoute.Class (routeEncoder)
-import Emanote.Source.Loc qualified as Loc
-import Emanote.Source.Patch qualified as Patch
-import Emanote.Source.Pattern qualified as Pattern
-import Emanote.View qualified as View
 import Emanote.View.Common (generatedCssFile)
 import Main.Utf8 (withUtf8)
-import Paths_emanote qualified
 import Relude
 import Spec qualified
 import System.Environment qualified as Env
@@ -44,27 +34,7 @@ run :: CLI.Cli -> IO ()
 run cli = do
   let emaCli = CLI.emaCli cli
   genPaths <-
-    Ema.runSiteWithCli emaCli $
-      Site
-        { siteName = "emanote",
-          siteRender = SiteRender $ \m r -> do
-            pure $ View.render m r,
-          siteRouteEncoder = routeEncoder,
-          siteModelManager =
-            ModelManager $ do
-              cliAct <- askCLIAction
-              enc <- askRouteEncoder
-              defaultLayer <- Loc.defaultLayer <$> liftIO Paths_emanote.getDataDir
-              instanceId <- liftIO UUID.nextRandom
-              let layers = one defaultLayer <> Loc.userLayers (CLI.layers cli)
-              lift $
-                Emanote.emanate
-                  layers
-                  Pattern.filePatterns
-                  Pattern.ignorePatterns
-                  (Model.emptyModel cliAct enc instanceId)
-                  Patch.patchModel
-        }
+    Ema.runSiteWithCli emaCli $ mkEmanoteSite cli
   case Ema.CLI.action emaCli of
     Some (Ema.CLI.Generate outPath) -> do
       let cssPath = outPath </> generatedCssFile
