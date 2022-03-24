d4b1984b7f7b8f7cc9ca2dbd6902a3b3f30aa2f0
Sridhar Ratnakumar <srid@srid.ca>
2022-03-24 15:52:50 -0400

    gen: Report and fail on broken links (#246)

--- src/Emanote.hs
+++ src/Emanote.hs
@@ -2,9 +2,11 @@
 
 module Emanote (run) where
 
-import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Monad.Logger (runStderrLoggingT, runStdoutLoggingT)
+import Control.Monad.Writer.Strict
 import Data.Default (def)
 import Data.Dependent.Sum (DSum ((:=>)))
+import Data.Map.Strict qualified as Map
 import Ema
   ( CanGenerate (..),
     CanRender (..),
@@ -14,11 +16,14 @@ import Ema
   )
 import Ema.CLI qualified
 import Emanote.CLI qualified as CLI
+import Emanote.Model.Link.Rel (ResolvedRelTarget (..))
 import Emanote.Model.Type qualified as Model
+import Emanote.Prelude (logE, logW)
 import Emanote.Route.SiteRoute.Class (emanoteGeneratableRoutes, emanoteRouteEncoder)
 import Emanote.Route.SiteRoute.Type (SiteRoute)
 import Emanote.Source.Dynamic (emanoteModelDynamic)
 import Emanote.View.Common (generatedCssFile)
+import Emanote.View.Export qualified as Export
 import Emanote.View.Template qualified as View
 import Optics.Core ((%), (.~))
 import Relude
@@ -43,10 +48,28 @@ instance HasModel SiteRoute where
 run :: CLI.Cli -> IO ()
 run cli = do
   Ema.runSiteWithCli @SiteRoute (CLI.emaCli cli) cli >>= \case
-    Ema.CLI.Generate outPath :=> Identity genPaths ->
+    (model0, Ema.CLI.Generate outPath :=> Identity genPaths) -> do
       compileTailwindCss outPath genPaths
+      checkBrokenLinks model0
     _ ->
       pure ()
+
+checkBrokenLinks :: Model.Model -> IO ()
+checkBrokenLinks model = runStderrLoggingT $ do
+  ((), res :: Sum Int) <- runWriterT $
+    forM_ (Map.toList $ Export.modelRels model) $ \(noteRoute, rels) ->
+      forM_ rels $ \(Export.Link urt rrt) ->
+        case rrt of
+          RRTFound _ -> pure ()
+          RRTMissing -> do
+            logW $ "Broken link: " <> show noteRoute <> " -> " <> show urt
+            tell 1
+          RRTAmbiguous _ -> do
+            logW $ "Ambiguous link: " <> show noteRoute <> " -> " <> show urt
+            tell 1
+  unless (res == 0) $ do
+    logE $ "Found " <> show (getSum res) <> " broken links! Emanote generated the site, but the generated site has broken links."
+    exitFailure
 
 compileTailwindCss :: MonadUnliftIO m => FilePath -> [FilePath] -> m ()
 compileTailwindCss outPath genPaths = do
--- src/Emanote/View/Export.hs
+++ src/Emanote/View/Export.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE DeriveAnyClass #-}
 
-module Emanote.View.Export (renderGraphExport) where
+module Emanote.View.Export
+  ( renderGraphExport,
+    Link (..),
+    modelRels,
+  )
+where
 
 import Data.Aeson (ToJSON)
 import Data.Aeson qualified as Aeson
@@ -62,17 +67,20 @@ renderGraphExport model =
                       meta_
                       (fromMaybe [] $ Map.lookup k rels)
             )
-      rels =
-        Map.fromListWith (<>) $
-          M.modelNoteRels model <&> \rel ->
-            let from_ = lmlRouteKey $ rel ^. Rel.relFrom
-                to_ = rel ^. Rel.relTo
-                toTarget =
-                  Resolve.resolveUnresolvedRelTarget model to_
-                    <&> SR.siteRouteUrlStatic model
-             in (from_, one $ Link to_ toTarget)
+      rels = modelRels model & Map.mapKeys lmlRouteKey
       export = Export currentVersion notes_
    in Aeson.encode export
+
+modelRels :: Model -> Map LMLRoute [Link]
+modelRels model =
+  Map.fromListWith (<>) $
+    M.modelNoteRels model <&> \rel ->
+      let from_ = rel ^. Rel.relFrom
+          to_ = rel ^. Rel.relTo
+          toTarget =
+            Resolve.resolveUnresolvedRelTarget model to_
+              <&> SR.siteRouteUrlStatic model
+       in (from_, one $ Link to_ toTarget)
 
 -- An unique key to represent this LMLRoute in the exported JSON
 --
