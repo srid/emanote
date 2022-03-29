b40632477c8cc54f12489db760946e1201a96e44
Sridhar Ratnakumar <srid@srid.ca>
2022-03-29 10:53:32 -0400

    Replace tasty with hedgehog discover

--- src/Spec.hs
+++ src/Spec.hs
@@ -1,27 +1,15 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Spec (main) where
 
 import Emanote.Model.Link.RelSpec qualified as RelSpec
 import Emanote.Model.QuerySpec qualified as QuerySpec
+import Hedgehog.Main (defaultMain)
 import Relude
-import Test.Tasty
-import Test.Tasty.Hedgehog (
-  HedgehogShrinkLimit (..),
-  HedgehogTestLimit (..),
- )
 
 main :: IO ()
 main = do
-  defaultMain tests
-
--- | Number of successful tests for each hedgehog property.
-limit :: HedgehogTestLimit
-limit = HedgehogTestLimit (Just 10)
-
-tests :: TestTree
-tests =
-  localOption limit . localOption (HedgehogShrinkLimit (Just 2)) $
-    testGroup
-      "Tests"
-      [ QuerySpec.spec
-      , RelSpec.spec
-      ]
+  defaultMain
+    [ QuerySpec.tests
+    , RelSpec.tests
+    ]
