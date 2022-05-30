c58c1d89b0fcd5acfbd429441986f695362d2dbd
Sridhar Ratnakumar <srid@srid.ca>
2022-05-30 19:05:37 -0400

    nix: Update haskell-flake, ema (use our nixpkgs)

--- flake.nix
+++ flake.nix
@@ -5,8 +5,6 @@
     extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
   };
   inputs = {
-    ema.url = "github:srid/ema/multisite";
-    nixpkgs.follows = "ema/nixpkgs";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -14,12 +12,14 @@
     haskell-flake.url = "github:srid/haskell-flake";
 
     # Haskell dependency overrides
-    tailwind-haskell.url = "github:srid/tailwind-haskell/master";
-    tailwind-haskell.inputs.ema.follows = "ema";
+    ema.url = "github:srid/ema/multisite";
+    ema.flake = false;
     pandoc-link-context.url = "github:srid/pandoc-link-context/master";
     pandoc-link-context.flake = false;
     heist.url = "github:srid/heist/emanote";
     heist.flake = false;
+    tailwind-haskell.url = "github:srid/tailwind-haskell/master";
+    tailwind-haskell.inputs.ema.follows = "ema";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, haskell-flake, ... }:
     flake-parts.lib.mkFlake { inherit self; } {
@@ -29,7 +29,8 @@
         ./flake-module.nix
       ];
       perSystem = { pkgs, system, inputs', self', ... }: {
-        haskellProjects.emanote = {
+        haskellProjects.default = {
+          root = ./.;
           buildTools = hp: {
             inherit (pkgs)
               treefmt
@@ -40,12 +41,12 @@
             tailwind-haskell = inputs'.tailwind-haskell.packages.tailwind;
           };
           overrides = self: super: with pkgs.haskell.lib; {
-            ema = inputs'.ema.packages.default;
             tailwind = inputs'.tailwind-haskell.packages.tailwind;
             heist-emanote = dontCheck (self.callCabal2nix "heist-emanote" inputs.heist { });
             ixset-typed = doJailbreak (dontCheck (self.callHackage "ixset-typed" "0.5.1.0" { })); # Broken on nixpkgs
           };
           source-overrides = {
+            ema = inputs.ema;
             pandoc-link-context = inputs.pandoc-link-context;
           };
         };
