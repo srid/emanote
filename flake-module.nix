8cc42f81eee75d5c47aaaf2b2c0dcdc518301a0e
Sridhar Ratnakumar <srid@srid.ca>
2022-05-30 16:14:40 -0400

    cf

--- flake-module.nix
+++ flake-module.nix
@@ -36,6 +36,8 @@ in
                       };
                       # HACK: I can't seem to be able to convert `path` to a
                       # relative local path; so this is necessary.
+                      #
+                      # cf. https://discourse.nixos.org/t/converting-from-types-path-to-types-str/19405?u=srid
                       pathString = mkOption {
                         type = types.str;
                         description = ''Like `path` but local (not in Nix store)'';
