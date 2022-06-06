34290aaaa87bae834cb873af682e7077ca0f19b7
Sridhar Ratnakumar <srid@srid.ca>
2022-06-06 13:00:17 -0400

    Remove flake-compat (we no longer use nix-env)

--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
-# This file exists for legacy Nix installs (nix-build & nix-env)
-# https://nixos.wiki/wiki/Flakes#Using_flakes_project_from_a_legacy_Nix
-# You generally do *not* have to modify this ever.
+# TODO: Remove this after https://github.com/srid/haskell-template/issues/23
+
 (import
   (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
     fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
+      url = "https://github.com/edolstra/flake-compat/archive/b4a34015c698c7793d592d66adbab377907a2be8.tar.gz";
+      sha256 = "sha256-Z+s0J8/r907g149rllvwhb4pKi8Wam5ij0st8PwAh+E=";
     }
   )
   {
