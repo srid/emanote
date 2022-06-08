86daad5c9effd826e562463047b019488c80c618
Sridhar Ratnakumar <srid@srid.ca>
2022-06-07 21:59:14 -0400

    nix: Make docker.nix a flake-parts module

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,24 +1,35 @@
-# Builds a docker image containing the emanote executable
-{ pkgs, emanote }:
+# Add a package for docker image on Linux.
+{ self, config, lib, flake-parts-lib, ... }:
 
-pkgs.dockerTools.buildImage {
-  name = "sridca/emanote";
-  tag = "latest";
-  contents = [
-    emanote
-    # These are required for the GitLab CI runner
-    pkgs.coreutils
-    pkgs.bash_5
-  ];
+{
+  options = {
+    perSystem = flake-parts-lib.mkPerSystemOption
+      ({ config, self', inputs', pkgs, system, ... }: {
+        packages = pkgs.lib.optionalAttrs (system == "x86_64-linux")
+          {
+            dockerImage = pkgs.dockerTools.buildImage
+              {
+                name = "sridca/emanote";
+                tag = "latest";
+                contents = [
+                  self'.packages.default
+                  # These are required for the GitLab CI runner
+                  pkgs.coreutils
+                  pkgs.bash_5
+                ];
 
-  config = {
-    WorkingDir = "/data";
-    Volumes = {
-      "/data" = { };
-    };
-    Tmpfs = {
-      "/tmp" = { };
-    };
-    Cmd = [ "${emanote}/bin/emanote" ];
+                config = {
+                  WorkingDir = "/data";
+                  Volumes = {
+                    "/data" = { };
+                  };
+                  Tmpfs = {
+                    "/tmp" = { };
+                  };
+                  Cmd = [ "${pkgs.lib.getExe self'.packages.default}" ];
+                };
+              };
+          };
+      });
   };
 }
