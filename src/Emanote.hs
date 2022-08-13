d957084be1377b18676093a208dadec84d1dc5f6
Kalle Fagerberg <kalle.f8@proton.me>
2022-08-13 19:06:16 +0200

    lint fix

--- src/Emanote.hs
+++ src/Emanote.hs
@@ -50,7 +50,7 @@ instance IsRoute SiteRoute where
 instance EmaSite SiteRoute where
   type SiteArg SiteRoute = EmanoteConfig
   siteInput = emanoteSiteInput
-  siteOutput rp m r = View.emanoteSiteOutput rp m r
+  siteOutput = View.emanoteSiteOutput
 
 defaultEmanoteConfig :: CLI.Cli -> EmanoteConfig
 defaultEmanoteConfig cli =
