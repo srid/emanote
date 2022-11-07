70c89843dd5a1fdd3526e50313b3d7c61143e679
kukimik <kukimik@poczta.onet.pl>
2022-11-07 21:34:19 +0100

    Clarify naming.

--- src/Emanote/Pandoc/BuiltinFiltersSpec.hs
+++ src/Emanote/Pandoc/BuiltinFiltersSpec.hs
@@ -13,36 +13,36 @@ spec :: Spec
 spec =
   do
     describe "setExternalLinkicon" $ do
-      let run =
-            (>>=) . evalEither . fmap (query extractAttr . preparePandoc . snd) . parseMarkdown ""
-            where
-              extractAttr (Link (_, _, attrs) _ (_, _)) = snd <$> filter ((== "data-linkicon") . fst) attrs
-              extractAttr _ = []
+      let linkIconAttrs = fmap (query getDataLinkIconAttr) . parseAndRunFilters
+          parseAndRunFilters = fmap (preparePandoc . snd) . parseMarkdown ""
+          getDataLinkIconAttr (Link (_, _, attrs) _ (_, _)) = snd <$> filter ((== "data-linkicon") . fst) attrs
+          getDataLinkIconAttr _ = []
       it "respects user-specified data-linkicon attribute" . hedgehog $ do
-        run "[test](https://www.test.com){data-linkicon=abc}" (=== ["abc"])
-        run "[test](https://www.test.com){data-linkicon=\"\"}" (=== [""])
-        run "[[foo]]{data-linkicon=external}" (=== ["external"])
-        run "[abc](x/y/z){data-linkicon=none}" (=== ["none"])
+        linkIconAttrs "[test](https://www.test.com){data-linkicon=abc}" === pure ["abc"]
+        linkIconAttrs "[test](https://www.test.com){data-linkicon=\"\"}" === pure [""]
+        linkIconAttrs "[[foo]]{data-linkicon=external}" === pure ["external"]
+        linkIconAttrs "[abc](x/y/z){data-linkicon=none}" === pure ["none"]
       it "does not add attribute if link is internal" . hedgehog $ do
-        run "[[foo]]" (=== [])
-        run "[abc](x/y/z)" (=== [])
-        run "![[image.jpg]]" (=== [])
-        run "[![](path/to/image.png)](foo/bar/baz)" (=== [])
-        run "[[bar|baz]]" (=== [])
-        run "[`abc`](./test.txt)" (=== [])
-        run "[$$abc$$](/just/test#ing)" (=== [])
-        run "#tag" (=== [])
+        linkIconAttrs "[[foo]]" === pure []
+        linkIconAttrs "[abc](x/y/z)" === pure []
+        linkIconAttrs "![[image.jpg]]" === pure []
+        linkIconAttrs "[![](path/to/image.png)](foo/bar/baz)" === pure []
+        linkIconAttrs "[[bar|baz]]" === pure []
+        linkIconAttrs "[`abc`](./test.txt)" === pure []
+        linkIconAttrs "[$$abc$$](/just/test#ing)" === pure []
+        linkIconAttrs "#tag" === pure []
+        linkIconAttrs "[[abc|def]]" === pure []
       it "adds attribute if link is external and its description contains text, code or math" . hedgehog $ do
-        run "[Emanote](https://github.com/EmaApps/emanote)" (=== ["external"])
-        run "[`text`](http://somehost:1234/test)" (=== ["external"])
-        run "[$$E=mc^2$$](ssh://user@host.abc/~/path/)" (=== ["external"])
-        run "[![[picture.png]] ~~**`code`**~~](scheme://host:port/path?query)" (=== ["external"])
-        run "[==$$e^{i \\pi} + 1 = 0$$== ![[image.svg]]](git://host.xz/path/to/repo.git/)" (=== ["external"])
-        run "example@example.com" (=== ["external"])
-        run "https://www.test.gov" (=== ["external"])
-        run "[**![[image.jpg]] *qwerty* ![[image.jpg]]**](doi:10.1000/182)" (=== ["external"])
-        run "[:video_game:](bolo://hostname/)" (=== ["external"])
+        linkIconAttrs "[Emanote](https://github.com/EmaApps/emanote)" === pure ["external"]
+        linkIconAttrs "[`text`](http://somehost:1234/test)" === pure ["external"]
+        linkIconAttrs "[$$E=mc^2$$](ssh://user@host.abc/~/path/)" === pure ["external"]
+        linkIconAttrs "[![[picture.png]] ~~**`code`**~~](scheme://host:port/path?query)" === pure ["external"]
+        linkIconAttrs "[==$$e^{i \\pi} + 1 = 0$$== ![[image.svg]]](git://host.xz/path/to/repo.git/)" === pure ["external"]
+        linkIconAttrs "example@example.com" === pure ["external"]
+        linkIconAttrs "https://www.test.gov" === pure ["external"]
+        linkIconAttrs "[**![[image.jpg]] *qwerty* ![[image.jpg]]**](doi:10.1000/182)" === pure ["external"]
+        linkIconAttrs "[:video_game:](bolo://hostname/)" === pure ["external"]
       it "does not add attribute if link description contains no text, code or math" . hedgehog $ do
-        run "[](http://nothing.interesting.here)" (=== [])
-        run "[![[image.png]]](https://www.example.com)" (=== [])
-        run "[==*_**~~![[img.png]]~~**_*==](http://something.info)" (=== [])
+        linkIconAttrs "[](http://nothing.interesting.here)" === pure []
+        linkIconAttrs "[![[image.png]]](https://www.example.com)" === pure []
+        linkIconAttrs "[==*_**~~![[img.png]]~~**_*==](http://something.info)" === pure []
