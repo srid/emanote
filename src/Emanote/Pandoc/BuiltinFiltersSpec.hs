56489858d6692203d705b040f29e551ea0b22853
Sridhar Ratnakumar <srid@srid.ca>
2022-11-12 13:30:01 -0500

    naming should be descriptive

--- src/Emanote/Pandoc/BuiltinFiltersSpec.hs
+++ src/Emanote/Pandoc/BuiltinFiltersSpec.hs
@@ -13,37 +13,37 @@ spec :: Spec
 spec =
   do
     describe "setExternalLinkIcon" $ do
-      let linkIconAttrs = fmap (W.query $ getLinkAttr "data-linkicon") . parseEmanoteMarkdown
-          parseEmanoteMarkdown = fmap (preparePandoc . snd) . parseMarkdown "<test>"
+      let parseEmanoteMarkdown = fmap (preparePandoc . snd) . parseMarkdown "<test>"
+          getDataLinkIconAttrs = fmap (W.query $ getLinkAttr "data-linkicon") . parseEmanoteMarkdown
       it "respects user-specified data-linkicon attribute" . hedgehog $ do
-        linkIconAttrs "[test](https://www.test.com){data-linkicon=abc}" === Right ["abc"]
-        linkIconAttrs "[test](https://www.test.com){data-linkicon=\"\"}" === Right [""]
-        linkIconAttrs "[[foo]]{data-linkicon=external}" === Right ["external"]
-        linkIconAttrs "[abc](x/y/z){data-linkicon=none}" === Right ["none"]
+        getDataLinkIconAttrs "[test](https://www.test.com){data-linkicon=abc}" === Right ["abc"]
+        getDataLinkIconAttrs "[test](https://www.test.com){data-linkicon=\"\"}" === Right [""]
+        getDataLinkIconAttrs "[[foo]]{data-linkicon=external}" === Right ["external"]
+        getDataLinkIconAttrs "[abc](x/y/z){data-linkicon=none}" === Right ["none"]
       it "does not add attribute if link is internal" . hedgehog $ do
-        linkIconAttrs "[[foo]]" === Right []
-        linkIconAttrs "[abc](x/y/z)" === Right []
-        linkIconAttrs "![[image.jpg]]" === Right []
-        linkIconAttrs "[![](path/to/image.png)](foo/bar/baz)" === Right []
-        linkIconAttrs "[[bar|baz]]" === Right []
-        linkIconAttrs "[`abc`](./test.txt)" === Right []
-        linkIconAttrs "[$$abc$$](/just/test#ing)" === Right []
-        linkIconAttrs "#tag" === Right []
-        linkIconAttrs "[[abc|def]]" === Right []
+        getDataLinkIconAttrs "[[foo]]" === Right []
+        getDataLinkIconAttrs "[abc](x/y/z)" === Right []
+        getDataLinkIconAttrs "![[image.jpg]]" === Right []
+        getDataLinkIconAttrs "[![](path/to/image.png)](foo/bar/baz)" === Right []
+        getDataLinkIconAttrs "[[bar|baz]]" === Right []
+        getDataLinkIconAttrs "[`abc`](./test.txt)" === Right []
+        getDataLinkIconAttrs "[$$abc$$](/just/test#ing)" === Right []
+        getDataLinkIconAttrs "#tag" === Right []
+        getDataLinkIconAttrs "[[abc|def]]" === Right []
       it "adds attribute if link is external and its description contains text, code or math" . hedgehog $ do
-        linkIconAttrs "[Emanote](https://github.com/EmaApps/emanote)" === Right ["external"]
-        linkIconAttrs "[`text`](http://somehost:1234/test)" === Right ["external"]
-        linkIconAttrs "[$$E=mc^2$$](ssh://user@host.abc/~/path/)" === Right ["external"]
-        linkIconAttrs "[![[picture.png]] ~~**`code`**~~](scheme://host:port/path?query)" === Right ["external"]
-        linkIconAttrs "[==$$e^{i \\pi} + 1 = 0$$== ![[image.svg]]](git://host.xz/path/to/repo.git/)" === Right ["external"]
-        linkIconAttrs "example@example.com" === Right ["external"]
-        linkIconAttrs "https://www.test.gov" === Right ["external"]
-        linkIconAttrs "[**![[image.jpg]] *qwerty* ![[image.jpg]]**](doi:10.1000/182)" === Right ["external"]
-        linkIconAttrs "[:video_game:](bolo://hostname/)" === Right ["external"]
+        getDataLinkIconAttrs "[Emanote](https://github.com/EmaApps/emanote)" === Right ["external"]
+        getDataLinkIconAttrs "[`text`](http://somehost:1234/test)" === Right ["external"]
+        getDataLinkIconAttrs "[$$E=mc^2$$](ssh://user@host.abc/~/path/)" === Right ["external"]
+        getDataLinkIconAttrs "[![[picture.png]] ~~**`code`**~~](scheme://host:port/path?query)" === Right ["external"]
+        getDataLinkIconAttrs "[==$$e^{i \\pi} + 1 = 0$$== ![[image.svg]]](git://host.xz/path/to/repo.git/)" === Right ["external"]
+        getDataLinkIconAttrs "example@example.com" === Right ["external"]
+        getDataLinkIconAttrs "https://www.test.gov" === Right ["external"]
+        getDataLinkIconAttrs "[**![[image.jpg]] *qwerty* ![[image.jpg]]**](doi:10.1000/182)" === Right ["external"]
+        getDataLinkIconAttrs "[:video_game:](bolo://hostname/)" === Right ["external"]
       it "does not add attribute if link description contains no text, code or math" . hedgehog $ do
-        linkIconAttrs "[](http://nothing.interesting.here)" === Right []
-        linkIconAttrs "[![[image.png]]](https://www.example.com)" === Right []
-        linkIconAttrs "[==*_**~~![[img.png]]~~**_*==](http://something.info)" === Right []
+        getDataLinkIconAttrs "[](http://nothing.interesting.here)" === Right []
+        getDataLinkIconAttrs "[![[image.png]]](https://www.example.com)" === Right []
+        getDataLinkIconAttrs "[==*_**~~![[img.png]]~~**_*==](http://something.info)" === Right []
 
 getLinkAttr :: Text -> Inline -> [Text]
 getLinkAttr name (Link (_, _, attrs) _ (_, _)) =
