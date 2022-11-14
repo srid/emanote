364532e1d6bda8096be9451f71eab7471ea4e729
Sridhar Ratnakumar <srid@srid.ca>
2022-11-14 18:27:39 -0500

    close neuron; move 1 more file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,1 @@
-module Spec (main) where
-
-import Emanote.Model.Link.RelSpec qualified as RelSpec
-import Emanote.Model.QuerySpec qualified as QuerySpec
-import Emanote.Pandoc.BuiltinFiltersSpec qualified as BuiltinFiltersSpec
-import Relude
-import Test.Hspec (hspec)
-
-main :: IO ()
-main = do
-  hspec $ do
-    QuerySpec.spec
-    RelSpec.spec
-    BuiltinFiltersSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
