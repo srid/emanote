78f6a3cd3b82f5b06067b7bc2b790d1a5cf19bc4
Sridhar Ratnakumar <srid@srid.ca>
2022-12-24 08:42:26 -0500

    Nixify treefmt.toml

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-root.url = "github:srid/flake-root";
 
     # TODO: Dependencies waiting to go from Hackage to nixpkgs.
     heist-extra.url = "github:srid/heist-extra";
@@ -22,24 +24,23 @@
       systems = nixpkgs.lib.systems.flakeExposed;
       imports = [
         haskell-flake.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.treefmt-nix.flakeModule
         ./nix/emanote.nix
         ./nix/docker.nix
         ./nix/stork.nix
         ./nix/tailwind.nix
       ];
       perSystem = { pkgs, lib, config, ... }: {
+
+        # haskell-flake configuration
         haskellProjects.default = {
           packages.emanote.root = ./.;
           buildTools = hp: {
-            inherit (pkgs)
-              treefmt
-              nixpkgs-fmt;
-            inherit (hp)
-              cabal-fmt
-              ormolu;
             inherit (config.packages)
               stork;
-          };
+            treefmt = config.treefmt.build.wrapper;
+          } // config.treefmt.build.programs;
           source-overrides = {
             inherit (inputs)
               heist-extra heist;
@@ -77,6 +78,27 @@
                 ];
             };
         };
+
+        # treefmt-nix configuration
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+
+          programs.ormolu.enable = true;
+          programs.nixpkgs-fmt.enable = true;
+          programs.cabal-fmt.enable = true;
+
+          # We use fourmolu
+          settings.formatter.ormolu = {
+            options = [
+              "--ghc-opt"
+              "-XImportQualifiedPost"
+              "--ghc-opt"
+              "-XTypeApplications"
+            ];
+          };
+        };
+
         packages.default = config.packages.emanote;
         emanote = {
           package = config.packages.default;
