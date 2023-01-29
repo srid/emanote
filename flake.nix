215f0d12b52755e75dfb5556cc7005fa710697ce
Sridhar Ratnakumar <srid@srid.ca>
2023-01-29 11:31:26 -0500

    Switch over to using cachix in CI and flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "emanote";
   nixConfig = {
-    extra-substituters = "https://cache.srid.ca";
-    extra-trusted-public-keys = "cache.srid.ca:8sQkbPrOIoXktIwI0OucQBXod2e9fDjjoEZWn8OXbdo=";
+    extra-substituters = "https://srid.cachix.org";
+    extra-trusted-public-keys = "srid.cachix.org-1:3clnql5gjbJNEvhA/WQp7nrZlBptwpXnUk6JAv8aB2M=";
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
