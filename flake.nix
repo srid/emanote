b8cb44dea13d8d2ff6a501fd7a3b976270c73e44
Sridhar Ratnakumar <srid@srid.ca>
2023-02-13 13:33:51 -0500

    nix: Remove HCI

--- flake.nix
+++ flake.nix
@@ -124,8 +124,6 @@
       flake = {
         homeManagerModule = import ./nix/home-manager-module.nix;
         flakeModule = import ./nix/emanote.nix;
-        # CI configuration
-        herculesCI.ciSystems = [ "x86_64-linux" "aarch64-darwin" ];
       };
     };
 }
