04341b9586824bc3877af6adb627b0e7e3dbe4ac
Sridhar Ratnakumar <srid@srid.ca>
2024-11-07 20:14:44 -0500

    try darwin

--- nix/modules/flake-parts/container.nix
+++ nix/modules/flake-parts/container.nix
@@ -16,10 +16,10 @@
     in
     {
       # Load the container locally with: `nix build .#container && ./result | podman load`
-      packages = lib.optionalAttrs pkgs.stdenv.isLinux { container = container; };
+      packages = { container = container; };
 
       # Run this script in the CI to publish a new image
-      apps = lib.optionalAttrs pkgs.stdenv.isLinux {
+      apps = {
         publish-container-release.program = pkgs.writeShellScriptBin "emanote-release" ''
           set -e
           export PATH=$PATH:${pkgs.gzip}/bin:${pkgs.skopeo}/bin
