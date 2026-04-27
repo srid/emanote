2822f0c4db0abb435173b3a2a56dc0b406e041fe
Sridhar Ratnakumar <srid@srid.ca>
2026-04-26 22:14:48 -0400

    refactor(hickey): preserve nested link labels
    Nested labels flattened: unwrap nested link targets while preserving label inline structure.

--- emanote/src/Emanote/Pandoc/Markdown/Parser.hs
+++ emanote/src/Emanote/Pandoc/Markdown/Parser.hs
@@ -54,20 +54,50 @@ parseMarkdown fp =
         <> CE.taskListSpec
 
 -- commonmark-hs can emit illegal nested links when the autolink extension sees
--- a URI inside explicit link text. Keep the outer link and collapse inner links
--- back to their displayed text.
+-- a URI inside explicit link text. Keep the outer link and unwrap inner links
+-- back to their displayed label structure.
 removeNestedLinks :: B.Pandoc -> B.Pandoc
 removeNestedLinks =
-  W.walk $ \case
-    B.Link attr inlines target ->
-      B.Link attr (plainifyLinks inlines) target
-    x ->
-      x
+  W.walk sanitizeLink
   where
-    plainifyLinks :: [B.Inline] -> [B.Inline]
-    plainifyLinks =
-      W.walk $ \case
+    sanitizeLink :: B.Inline -> B.Inline
+    sanitizeLink = \case
+      B.Link attr inlines target ->
+        B.Link attr (sanitizeLinkLabel inlines) target
+      x ->
+        x
+
+    sanitizeLinkLabel :: [B.Inline] -> [B.Inline]
+    sanitizeLinkLabel =
+      concatMap $ \case
         B.Link _ inlines _ ->
-          B.Str $ WL.plainify inlines
-        x ->
-          x
+          sanitizeLinkLabel inlines
+        inline ->
+          one $ sanitizeInlineLabel inline
+
+    sanitizeInlineLabel :: B.Inline -> B.Inline
+    sanitizeInlineLabel = \case
+      B.Emph inlines ->
+        B.Emph $ sanitizeLinkLabel inlines
+      B.Underline inlines ->
+        B.Underline $ sanitizeLinkLabel inlines
+      B.Strong inlines ->
+        B.Strong $ sanitizeLinkLabel inlines
+      B.Strikeout inlines ->
+        B.Strikeout $ sanitizeLinkLabel inlines
+      B.Superscript inlines ->
+        B.Superscript $ sanitizeLinkLabel inlines
+      B.Subscript inlines ->
+        B.Subscript $ sanitizeLinkLabel inlines
+      B.SmallCaps inlines ->
+        B.SmallCaps $ sanitizeLinkLabel inlines
+      B.Quoted quoteType inlines ->
+        B.Quoted quoteType $ sanitizeLinkLabel inlines
+      B.Cite citations inlines ->
+        B.Cite citations $ sanitizeLinkLabel inlines
+      B.Image attr inlines target ->
+        B.Image attr (sanitizeLinkLabel inlines) target
+      B.Span attr inlines ->
+        B.Span attr $ sanitizeLinkLabel inlines
+      inline ->
+        inline
